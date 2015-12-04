8c0c7f0286f7a64f5db3a7e7df536fce3643f3bf
MattCordier <mcordier24@gmail.com>
2015-12-04 09:41:23 -0600

    pushing staging to AWS through Github

--- customer/create_customer.php
+++ customer/create_customer.php
@@ -1,7 +1,7 @@
 <?php
      
-    require '../ecomm_connect.php';
-    echo "WTF!!";
+    // require '../ecomm_connect.php';
+    // echo "WTF!!";
  
     if ( !empty($_POST)) {
         // keep track validation errors
