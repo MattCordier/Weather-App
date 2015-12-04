bf9feb867e72b0e7de7cde9301465b9db33cca6f
MattCordier <mcordier24@gmail.com>
2015-12-04 17:09:18 -0600

    pushing staging to AWS through Github

--- tag/index.php
+++ tag/index.php
@@ -32,11 +32,11 @@
                             echo '<td>'. $row['name'] . '</td>';
                             
                             echo '<td width=250>';
-                                echo '<a class="btn" href="payment_read.php?id='.$row['id'].'">Read</a>';
+                                echo '<a class="btn" href="tag_read.php?id='.$row['id'].'">Read</a>';
                                 echo ' ';
-                                echo '<a class="btn btn-success" href="payment_update.php?id='.$row['id'].'">Update</a>';
+                                echo '<a class="btn btn-success" href="tag_update.php?id='.$row['id'].'">Update</a>';
                                 echo ' ';
-                                echo '<a class="btn btn-danger" href="payment_delete.php?id='.$row['id'].'">Delete</a>';
+                                echo '<a class="btn btn-danger" href="tag_delete.php?id='.$row['id'].'">Delete</a>';
                                 echo '</td>';
                             echo '</tr>';
                    }
