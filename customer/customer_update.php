534ba3c4c942c72f03234df5eb0d29a185e0fa30
Matt Cordier <mcordier24@gmail.com>
2015-12-04 02:57:25 +0000

    initial commit

---
+++ customer/customer_update.php
@@ -0,0 +1,232 @@
+<?php
+	require '../ecomm_connect.php';
+
+	$id = null;
+	if ( !empty($_GET['id'])) {
+		$id = $_REQUEST['id'];
+	}
+
+	if ( null==$id ) {
+		header("Location: index.php");
+	}
+
+	if ( !empty($_POST)) {
+		$firstnameError = null;
+        $lastnameError = null;
+        $phoneError = null;
+        $dobError = null;
+        $usernameError = null;
+        $passwordError = null;
+        $genderError = null;
+        $permissionError = null;
+        $emailError = null;
+
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $phone = $_POST['phone'];
+        $dob = $_POST['dob'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $gender = $_POST['gender'];
+        $permission = $_POST['permission'];
+        $email = $_POST['email'];
+
+          // validate input
+        $valid = true;
+        if (empty($firstname)) {
+            $nameError = 'Please enter First Name';
+            $valid = false;
+        }
+
+        if (empty($lastname)) {
+            $nameError = 'Please enter Last Name';
+            $valid = false;
+        }
+         
+        if (empty($phone)) {
+            $phoneError = 'Please enter Phone Number';
+            $valid = false;
+        } 
+         
+        if (empty($dob)) {
+            $dobError = 'Please enter Date of Birth';
+            $valid = false;
+        }
+
+        if (empty($username)) {
+            $usernameError = 'Please enter Username';
+            $valid = false;
+        }
+
+        if (empty($password)) {
+            $passwordError = 'Please enter Password';
+            $valid = false;
+        }
+
+        if (empty($gender)) {
+            $genderError = 'Please enter Gender';
+            $valid = false;
+        }
+
+        if (empty($permission)) {
+            $permissionError = 'Please enter permission';
+            $valid = false;
+        }
+
+        if (empty($email)) {
+            $emailError = 'Please enter Email';
+            $valid = false;
+        } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+            $emailError = 'Please enter a valid Email Address';
+            $valid = false;
+        }
+
+        //update data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "UPDATE customer set firstname = ?, lastname= ?, phone = ?, dob = ?, username = ?, password = ?, gender = ?, permission = ?, email = ? WHERE id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($firstname, $lastname, $phone, $dob, $username, $password, $gender, $permission, $email, $id));
+            Database::disconnect();
+            header("Location: index.php");
+        }
+	} else {
+		$pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM customer where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        $firstname = $data['firstname'];
+        $lastname = $data['lastname'];
+        $phone = $data['phone'];
+        $dob = $data['dob'];
+        $username = $data['username'];
+        $password = $data['password'];
+        $gender = $data['gender'];
+        $permission = $data['permission'];
+        $email = $data['email'];
+        Database::disconnect();	
+	}
+?>
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Update a Customer</h3>
+                    </div>
+             
+                    <form class="form-horizontal" action="customer_update.php?id=<?php echo $id?>" method="post">
+
+
+                      <div class="control-group <?php echo !empty($firstnameError)?'error':'';?>">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input name="firstname" type="text"  placeholder="Name" value="<?php echo !empty($firstname)?$firstname:'';?>">
+                            <?php if (!empty($firstnameError)): ?>
+                                <span class="help-inline"><?php echo $firstnameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($lastnameError)?'error':'';?>">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input name="lastname" type="text"  placeholder="Last Name" value="<?php echo !empty($lastname)?$lasstname:'';?>">
+                            <?php if (!empty($lasstnameError)): ?>
+                                <span class="help-inline"><?php echo $lasstnameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+
+
+                      <div class="control-group <?php echo !empty($phoneError)?'error':'';?>">
+                        <label class="control-label">Phone</label>
+                        <div class="controls">
+                            <input name="phone" type="text" placeholder="Phone Number" value="<?php echo !empty($phone)?$phone:'';?>">
+                            <?php if (!empty($phoneError)): ?>
+                                <span class="help-inline"><?php echo $phoneError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+
+                      <div class="control-group <?php echo !empty($dobError)?'error':'';?>">
+                        <label class="control-label">Date of Birth</label>
+                        <div class="controls">
+                            <input name="dob" type="text"  placeholder="Date of Birth" value="<?php echo !empty($dob)?$dob:'';?>">
+                            <?php if (!empty($dobError)): ?>
+                                <span class="help-inline"><?php echo $dobError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($usernameError)?'error':'';?>">
+                        <label class="control-label">Username</label>
+                        <div class="controls">
+                            <input name="username" type="text"  placeholder="Username" value="<?php echo !empty($username)?$username:'';?>">
+                            <?php if (!empty($usernameError)): ?>
+                                <span class="help-inline"><?php echo $usernameError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
+                        <label class="control-label">Password</label>
+                        <div class="controls">
+                            <input name="password" type="text"  placeholder="Password" value="<?php echo !empty($password)?$password:'';?>">
+                            <?php if (!empty($passwordError)): ?>
+                                <span class="help-inline"><?php echo $passwordError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($genderError)?'error':'';?>">
+                        <label class="control-label">Gender</label>
+                        <div class="controls">
+                            <input name="gender" type="text"  placeholder="Gender" value="<?php echo !empty($gender)?$gender:'';?>">
+                            <?php if (!empty($genderError)): ?>
+                                <span class="help-inline"><?php echo $genderError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($permissionError)?'error':'';?>">
+                        <label class="control-label">Permission</label>
+                        <div class="controls">
+                            <input name="permission" type="text"  placeholder="Permission" value="<?php echo !empty($permission)?$permission:'';?>">
+                            <?php if (!empty($permissionError)): ?>
+                                <span class="help-inline"><?php echo $permissionError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
+                        <label class="control-label">Email</label>
+                        <div class="controls">
+                            <input name="email" type="text"  placeholder="Email" value="<?php echo !empty($email)?$email:'';?>">
+                            <?php if (!empty($emailError)): ?>
+                                <span class="help-inline"><?php echo $emailError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Update</button>
+                          <a class="btn" href="index.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
---
+++ payment/index.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
+ 
+<body>
+    <div class="container">
+            <div class="row">
+                <h3>Payment</h3>
+            </div>
+            <div class="row">
+                <p>
+                    <a href="create_customer.php" class="btn btn-success">Create</a>
+                </p>
+                <table class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Full Name</th>
+                      <th>Card Name</th>
+                      <th>Card Number</th>
+                      <th>Security Code</th>
+                      <th>Expiration Month</th>
+                      <th>Expiration Year</th>
+                      <th>Payment Type</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php
+                   include '../ecomm_connect.php';
+                   $pdo = Database::connect();
+                   $sql = 'SELECT * FROM payment ORDER BY id DESC';
+                   foreach ($pdo->query($sql) as $row) {
+                            echo '<tr>';
+                            echo '<td>'. $row['full_name'] . '</td>';
+                            echo '<td>'. $row['card_number'] . '</td>';
+                            echo '<td>'. $row['card_security'] . '</td>';
+                            echo '<td>'. $row['expires_month'] . '</td>';
+                            echo '<td>'. $row['expired_year'] . '</td>';
+                            echo '<td>'. $row['payment_type'] . '</td>';
+                            echo '<td width=250>';
+                                echo '<a class="btn" href="customer_read.php?id='.$row['id'].'">Read</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-success" href="customer_update.php?id='.$row['id'].'">Update</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-danger" href="customer_delete.php?id='.$row['id'].'">Delete</a>';
+                                echo '</td>';
+                            echo '</tr>';
+                   }
+                   Database::disconnect();
+                  ?>
+                  </tbody>
+            </table>
+        </div>
+    </div> <!-- /container -->
+  </body>
+</html>
+
