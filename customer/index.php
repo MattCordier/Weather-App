efe7de09b171c5a1577da8e7b33373382cb11c4f
MattCordier <mcordier24@gmail.com>
2015-12-04 11:05:03 -0600

    pushing staging to AWS through Github

--- customer/index.php
+++ customer/index.php
@@ -10,7 +10,7 @@
 <body>
     <div class="container">
             <div class="row">
-                <h3>Customer Table</h3>
+                <h3>Customers</h3>
             </div>
             <div class="row">
             	<p>
