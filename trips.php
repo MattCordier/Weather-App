cca30335b40b81614dd22174e32b9564ff61ea0e
MattCordier <mcordier24@gmail.com>
2015-12-08 17:44:29 -0600

    pushing staging to AWS through Github

--- trips.php
+++ trips.php
@@ -10,32 +10,24 @@
             </div>
 
             <div class="row">
-                <p>
-                    <a href="trip_create.php" class="btn btn-success">Create</a>
-                </p>
-                <table class="table table-striped table-bordered">
-                  
+                
+                  <!-- 
                   <?php
                    include '../ecomm_connect.php';
                    $pdo = Database::connect();
                    $sql = 'SELECT name FROM style ORDER BY name DESC';
                    foreach ($pdo->query($sql) as $row) {
-                            echo '<td>'. $row['name'] . '</td>'; 
-                            echo '<td>'. $row['description'] . '</td>';
-                            echo '<td>'. $row['style.name'] . '</td>';
-                            echo '<td>'. $row['destination_id'] . '</td>';
+                            $row['name'] 
+                             $row['description'] 
+                             $row['style.name'] 
+                            $row['destination_id'] 
                             
-                            echo '<select>'. 
-  								<option value="volvo">Volvo</option>
-								<option value="saab">Saab</option>
-								<option value="mercedes">Mercedes</option>
-								<option value="audi">Audi</option>
-							</select>
+                            
                    }
                    Database::disconnect();
-                  ?>
-                  </tbody>
-            </table>
+                  ?> -->
+                  
+           
         </div>
     </div> <!-- /container -->
 
