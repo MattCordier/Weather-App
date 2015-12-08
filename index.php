a33ed1e0ef357d60dba918fc04d17d1e34e3bbac
MattCordier <mcordier24@gmail.com>
2015-12-07 21:49:49 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <!DOCTYPE html>
 <html>
-
+	
 <body>
 <?php require "navigation.php";?>
+<!--Flex Slider CSS-->
+      <link rel="stylesheet" href="assets/flexslider.css" type="text/css">
+<!--Flex Slider jQuery-->
+      <script src="assets/jquery.flexslider-min.js"></script>
 
-<h1>Wh00t!!</h1>
+      <!--Flex Slider Script-->
+      <script type="text/javascript" charset="utf-8">
+          $(window).load(function() {
+            $('.flexslider').flexslider();
+          });
+      </script>
+	<div class="container-fluid">
+    <div class="flexslider">
+      <ul class="slides">
+        <li>
+          <img src="assets/img/apex_summit.jpg" title="winter sale event." alt="GoPup Winter Event."/>
+        </li>
+        <li>
+          <img src="assets/img/apex_stream.jpg" title="leash and collars." alt="GoPup Leashes and Collars."/>
+        </li>
+        <li>
+          <img src="assets/img/apex_plank_hiker.jpg" title="GoPup guarantee." alt="GoPup guarantee."/>
+        </li>
+      </ul>
+    </div> <!--end flexslider-->
 
 <?php require "footer.php";?>
 </body>
