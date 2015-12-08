15e33257424ff1ecca6c884abac44d0e70a8f47f
MattCordier <mcordier24@gmail.com>
2015-12-07 20:33:27 -0600

    pushing staging to AWS through Github

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <meta name="author" content="Matt Cordier">
 	    <meta name="description" content="">
-	    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css.map">
 	    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 	    <link rel="stylesheet" href="assets/css/normalize.css">
 	    <link rel="stylesheet" type="text/styles" href="assets/css/styles.css">
