9a1dff7752944d753415b0974eb1b5a03b10db53
MattCordier <mcordier24@gmail.com>
2015-12-08 14:12:09 -0600

    pushing staging to AWS through Github

--- CRUD/trip/index.php
+++ CRUD/trip/index.php
@@ -32,22 +32,22 @@
                   <?php
                    include '../ecomm_connect.php';
                    $pdo = Database::connect();
-                   $sql = 'SELECT name FROM trip ORDER BY id DESC';
+                   $sql = 'SELECT * FROM trip ORDER BY id DESC';
                    foreach ($pdo->query($sql) as $row) {
                             echo '<td>'. $row['name'] . '</td>';
-                            // echo '<td>'. $row['cost'] . '</td>';
-                            // echo '<td>'. $row['description'] . '</td>';
-                            // echo '<td>'. $row['style_id'] . '</td>';
-                            // echo '<td>'. $row['desitnation_id'] . '</td>';
+                            echo '<td>'. $row['cost'] . '</td>';
+                            echo '<td>'. $row['description'] . '</td>';
+                            echo '<td>'. $row['style_id'] . '</td>';
+                            echo '<td>'. $row['desitnation_id'] . '</td>';
                             
-                            // echo '<td width=250>';
-                            //     echo '<a class="btn" href="trip_read.php?id='.$row['id'].'">Read</a>';
-                            //     echo ' ';
-                            //     echo '<a class="btn btn-success" href="trip_update.php?id='.$row['id'].'">Update</a>';
-                            //     echo ' ';
-                            //     echo '<a class="btn btn-danger" href="trip_delete.php?id='.$row['id'].'">Delete</a>';
-                            //     echo '</td>';
-                            // echo '</tr>';
+                            echo '<td width=250>';
+                                echo '<a class="btn" href="trip_read.php?id='.$row['id'].'">Read</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-success" href="trip_update.php?id='.$row['id'].'">Update</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-danger" href="trip_delete.php?id='.$row['id'].'">Delete</a>';
+                                echo '</td>';
+                            echo '</tr>';
                    }
                    Database::disconnect();
                   ?>
