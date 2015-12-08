d9f43b3081172f26e5c02612653593f0c8166289
MattCordier <mcordier24@gmail.com>
2015-12-08 17:41:08 -0600

    pushing staging to AWS through Github

--- trips.php
+++ trips.php
@@ -4,45 +4,33 @@
 <?php require "ecomm_connect.php";?>
 <body>
 <?php require "navigation.php";?>
- <div class="container">
+ <div class="container main-bg">
             <div class="row">
-                <h3>Trip</h3>
+                <h3>Select a Trip</h3>
             </div>
+
             <div class="row">
                 <p>
                     <a href="trip_create.php" class="btn btn-success">Create</a>
                 </p>
                 <table class="table table-striped table-bordered">
-                  <thead>
-                    <tr>
-                      <th>Trip Name</th>
-                      
-                      <th>Description</th>
-                      <th>Style</th>
-                      <th>Destination</th>
-
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
+                  
                   <?php
                    include '../ecomm_connect.php';
                    $pdo = Database::connect();
-                   $sql = 'SELECT * FROM trip ORDER BY id DESC';
+                   $sql = 'SELECT name FROM style ORDER BY name DESC';
                    foreach ($pdo->query($sql) as $row) {
                             echo '<td>'. $row['name'] . '</td>'; 
                             echo '<td>'. $row['description'] . '</td>';
                             echo '<td>'. $row['style.name'] . '</td>';
                             echo '<td>'. $row['destination_id'] . '</td>';
                             
-                            echo '<td width=250>';
-                                echo '<a class="btn" href="trip_read.php?id='.$row['id'].'">Read</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-success" href="trip_update.php?id='.$row['id'].'">Update</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-danger" href="trip_delete.php?id='.$row['id'].'">Delete</a>';
-                                echo '</td>';
-                            echo '</tr>';
+                            echo '<select>'. 
+  								<option value="volvo">Volvo</option>
+								<option value="saab">Saab</option>
+								<option value="mercedes">Mercedes</option>
+								<option value="audi">Audi</option>
+							</select>
                    }
                    Database::disconnect();
                   ?>
