beff82236710079e415c5b366dc5504c2b0beeee
MattCordier <mcordier24@gmail.com>
2015-12-08 12:31:27 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -27,13 +27,13 @@
 	    <h1 class="main-title">Hiking Tours</h1>
 
 	    <div class="row">
-	    	<div class="col-sm-4 col-xs-12">
+	    	<div class="col-xs-4">
 	    		<p>suck</p>
 	    	</div>
-	    	<div class="col-sm-4 col-xs-12">
+	    	<div class="col-xs-4">
 	    		<p>a</p>
 	    	</div>
-	    	<div class="col-sm-4 col-xs-12">
+	    	<div class="col-xs-4">
 	    		<p>duck</p>
 	    	</div>
 	    </div>
