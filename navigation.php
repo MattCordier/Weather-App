5c6de9b2353dd7955970d5c0a51b3e92e9a5a7e0
MattCordier <mcordier24@gmail.com>
2015-12-07 21:03:32 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -19,8 +19,9 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+        <li <a href="Home">Home </a></li>
+        <li><a href="Trips">Trips</a></li>
+        <li><a href="Search">Search</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu">
