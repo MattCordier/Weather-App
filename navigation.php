f9e9be6d189d8289e3097812208a62a355af575f
MattCordier <mcordier24@gmail.com>
2015-12-08 10:09:59 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -4,7 +4,7 @@
 ?>
 
 <nav class="navbar navbar-default conatiner">
-  <div>
+  <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -30,5 +30,5 @@
         <li><a href="login.php">Login</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
-  </div>
+  </div><!-- /.container -->
 </nav>
