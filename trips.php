371f0a056f504553ab716f6c132047315b2ca6ac
MattCordier <mcordier24@gmail.com>
2015-12-08 17:48:55 -0600

    pushing staging to AWS through Github

--- trips.php
+++ trips.php
@@ -11,7 +11,22 @@
 
             <div class="row">
                 
-                
+                <div class="col-xs-6">
+                	<select>
+					  <option value="volvo">Volvo</option>
+					  <option value="saab">Saab</option>
+					  <option value="mercedes">Mercedes</option>
+					  <option value="audi">Audi</option>
+					</select>
+                </div>
+                <div class="col-xs-6">
+                	<select>
+					  <option value="volvo">Volvo</option>
+					  <option value="saab">Saab</option>
+					  <option value="mercedes">Mercedes</option>
+					  <option value="audi">Audi</option>
+					</select>
+				</div>
                   
            
         </div>
