46e087b7c0336afade59a8f1abee055afb2434cc
MattCordier <mcordier24@gmail.com>
2015-12-08 18:21:08 -0600

    pushing staging to AWS through Github

--- trips.php
+++ trips.php
@@ -4,6 +4,11 @@
 <?php require "ecomm_connect.php";?>
 <body>
 <?php require "navigation.php";?>
+<?php
+	include '../ecomm_connect.php';
+	$pdo = Database::connect();
+?>
+
  <div class="container main-bg">
             <div class="row">
                 <h3>Select a Trip</h3>
@@ -15,19 +20,14 @@
                 	<div class="form-group">
 					  	<label for="sel1">Hiker Style:</label>
 					  	<select class="form-control" id="sel1">
+
 					  		<?php
-                   include '../ecomm_connect.php';
-                   $pdo = Database::connect();
-                   $sql = 'SELECT * FROM style ORDER BY name';
-                   foreach ($pdo->query($sql) as $row) {
-                            echo '<option>'. $row['name'] . '</option>';
-                   }
-                   Database::disconnect();
-                  ?>
-					    	<option>1</option>
-					    	<option>2</option>
-					    	<option>3</option>
-					    	<option>4</option>
+			                   $sql = 'SELECT * FROM style ORDER BY name';
+			                   foreach ($pdo->query($sql) as $row) {
+			                            echo '<option>'. $row['name'] . '</option>';
+			                   }
+			                ?>
+					    	
 					  	</select>
 					</div>
                 </div>
@@ -35,17 +35,22 @@
                 	<div class="form-group">
   						<label for="sel1">Destination:</label>
   						<select class="form-control" id="sel1">
-							<option>1</option>
-						    <option>2</option>
-						    <option>3</option>
-						    <option>4</option>
+							<?php
+			                   $sql = 'SELECT * FROM destination ORDER BY name';
+			                   foreach ($pdo->query($sql) as $row) {
+			                            echo '<option>'. $row['name'] . '</option>';
+			                   }
+			                ?>
 						</select>
-</div>
+					</div>
 				</div>
                   
            
         </div>
     </div> <!-- /container -->
+
+
+					  		<?php Database::disconnect(); ?>
 
 <?php require "footer.php";?>
 </body>
