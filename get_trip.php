fcd825bfc5b6588ae22bbd1e10a78147ddeac055
MattCordier <mcordier24@gmail.com>
2015-12-09 15:08:26 -0600

    pushing staging to AWS through Github

--- get_trip.php
+++ get_trip.php
@@ -1,6 +1,14 @@
 <?php
-	
-	echo 'yeah!!!!!!!!!!';
+	include 'ecomm_connect.php';
+                   $pdo = Database::connect();
+                   $sql = 'SELECT * FROM trip WHERE style_id = 1 ORDER BY id DESC';
+                   foreach ($pdo->query($sql) as $row) {
+                            echo 'p>'. $row['name'] . '</p>';
+                            echo 'p>'. $row['description'] . '</p>';
+                            echo 'p>'. $row['cost'] . '</p>';
+                           
+                   }
+                   Database::disconnect();
 
 ?>
 
