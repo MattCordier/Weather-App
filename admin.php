d456d6b9b10efaa8ddfa2ec237fa2e4f84782fa5
MattCordier <mcordier24@gmail.com>
2015-12-10 10:40:34 -0600

    pushing staging to AWS through Github

--- admin.php
+++ admin.php
@@ -12,24 +12,24 @@
 		$file = $_FILES['image']['tmp_name'];
 
 
-		if(!isset($file)){
-			echo "<p>". "Please Select an Image.". "</p>";
-		} else {
-			$image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
-			$image_name = $_FILES['image']['name'];
-			$image_size = fileimagesize($_FILES['image']['tmp_name']);
+		// if(!isset($file)){
+		// 	echo "<p>". "Please Select an Image.". "</p>";
+		// } else {
+		// 	$image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+		// 	$image_name = $_FILES['image']['name'];
+		// 	$image_size = fileimagesize($_FILES['image']['tmp_name']);
 
-			if ($image_size == FALSE)
-				echo "That's not an image";
-			 else (){
-				if (!$insert = mysql_query("INSERT INTO image VALUES ('','$image','$image')"))
-					echo "Problem uploading image.";
-				else{
-					$lastid = mysql_insert_id();
-					// echo "Image uploaded.<p />Your Image:<p /><img src=get.php?id=$lastid>";
-				}
-			}	
-		}
+		// 	if ($image_size == FALSE)
+		// 		echo "That's not an image";
+		// 	 else (){
+		// 		if (!$insert = mysql_query("INSERT INTO image VALUES ('','$image','$image')"))
+		// 			echo "Problem uploading image.";
+		// 		else{
+		// 			$lastid = mysql_insert_id();
+		// 			echo "Image uploaded.<p />Your Image:<p /><img src=get.php?id=$lastid>";
+		// 		}
+		// 	}	
+		// }
 
 
 
