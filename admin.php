94c8b86bd2bec6ea08bdb83b37181da27ee4935f
MattCordier <mcordier24@gmail.com>
2015-12-10 11:22:47 -0600

    pushing staging to AWS through Github

--- admin.php
+++ admin.php
@@ -16,11 +16,12 @@
 			echo "Please Upload an Image.";
 		}
 		else{
-			echo "yippee!";
+			
 			$image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
 			$image_name = $_FILES['image']['name'];
 			$image_size = fileimagesize($_FILES['image']['tmp_name']);
-
+			echo $image_name;
+			echo $image_size;
 		}
 		
 		// } else {
