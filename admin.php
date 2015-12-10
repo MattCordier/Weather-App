d832ba906cee5e395a105ff9ae77ec8ae5247dae
MattCordier <mcordier24@gmail.com>
2015-12-10 09:46:55 -0600

    pushing staging to AWS through Github

---
+++ admin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<?php require "header.php";?>
+
+<body>
+<?php require "navigation.php";?>
+	<form action="admin.php" method="POST" enctype="multipart/form-data">File:
+		<input type="file" name="image">
+		<input type="submit" value="Upload">
+	</form>
+		
+
+<?php require "footer.php";?>
+</body>
+
+</html>
