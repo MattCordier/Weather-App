a0d314675f0199351841e2b7ee18f6b7a97d708b
MattCordier <mcordier24@gmail.com>
2015-12-10 11:33:02 -0600

    pushing staging to AWS through Github

--- admin.php
+++ admin.php
@@ -20,23 +20,23 @@
 			$image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
 			$image_name = $_FILES['image']['name'];
 			$image_size = fileimagesize($_FILES['image']['tmp_name']);
-			echo $file;
+			
 
 		}
 		
-		// } else {
+		} else {
 
-		// 	if ($image_size == FALSE)
-		// 		echo "That's not an image";
-		// 	 else (){
-		// 		if (!$insert = mysql_query("INSERT INTO image VALUES ('','$image','$image')"))
-		// 			echo "Problem uploading image.";
-		// 		else{
-		// 			$lastid = mysql_insert_id();
-		// 			echo "Image uploaded.<p />Your Image:<p /><img src=get.php?id=$lastid>";
-		// 		}
-		// 	}	
-		// }
+			if ($image_size == FALSE)
+				echo "That's not an image";
+			 else (){
+				if (!$insert = mysql_query("INSERT INTO image VALUES ('','$image','$image')"))
+					echo "Problem uploading image.";
+				else{
+					$lastid = mysql_insert_id();
+					echo "Image uploaded.<p />Your Image:<p /><img src=get.php?id=$lastid>";
+				}
+			}	
+		}
 
 
 
