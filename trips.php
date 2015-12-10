aa44eaa258f8f3a71c26e118c14246d4463b33f1
MattCordier <mcordier24@gmail.com>
2015-12-09 19:18:18 -0600

    pushing staging to AWS through Github

--- trips.php
+++ trips.php
@@ -77,7 +77,7 @@
     		var style = $("#style").val();
     		console.log("retrieving filtered results for " + destination + " & " + style);
     		$.get("get_trip.php?style=" + style + "&destination=" + destination, function(data){
-    			if(data !== null && data !==''){
+    			if(data !== null && data.length > 5){
     				$('#trips').html(data);
     			} else {
 					$('#trips').html("none");
