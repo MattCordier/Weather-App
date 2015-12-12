dbba38d21a69658911282f20d02275b76e9cb3a9
MattCordier <mcordier24@gmail.com>
2015-12-12 12:06:22 -0600

    pushing staging to AWS through Github

--- trips.php
+++ trips.php
@@ -4,51 +4,46 @@
 
 <body>
 <?php require "navigation.php";?>
-
-
  	<div class="container main-bg">
-            <div class="row">
-                <h3>Select a Trip</h3>
-            </div>
+        <div class="row">
+            <h3>Select a Trip</h3>
+        </div>
 
-            <div class="row">
-                
-                <div class="col-xs-6">
-                	<div class="form-group">
-					  	<label for="sel1">Hiker Style:</label>
-					  	<select id="style" class="form-control">
-					  		<option value="all" selected="selected"> All </option>
-					  		<?php
-					  			 
-			                   	$sql = 'SELECT * FROM style ORDER BY name';
-			                   	foreach ($pdo->query($sql) as $row) {
-			                            echo '<option value=" ' .$row['id']. ' ">'. $row['name'] . '</option>';
-			                   }
-			                ?>
-					    	
-					  	</select>
-					</div>
-                </div>
-                <div class="col-xs-6">
-                	<div class="form-group">
-  						<label for="sel1">Destination:</label>
-  						<select id="destination" class="form-control">
-  							<option value="all" selected="selected"> All </option>
-							<?php
-			                   $sql = 'SELECT * FROM destination ORDER BY name';
-			                   foreach ($pdo->query($sql) as $row) {
-			                            echo '<option value=" ' .$row['id']. ' ">'. $row['name'] . '</option>';
-			                   }
-			                ?>
-						</select>
-					</div>
-				</div>   
-        	</div><!--end row-->
-        	<div class="row">
-        		<div id="trips" class="col-xs-12">
+        <div class="row">
+            <div class="col-xs-6">
+                <div class="form-group">
+					<label for="sel1">Hiker Style:</label>
+					  <select id="style" class="form-control">
+					  	<option value="all" selected="selected"> All </option>
+		<?php		  			 
+			$sql = 'SELECT * FROM style ORDER BY name';
+			foreach ($pdo->query($sql) as $row) {
+			    echo '<option value=" ' .$row['id']. ' ">'. $row['name'] . '</option>';
+			}
+		?>
+					  </select>
+				</div>
+            </div>
+            <div class="col-xs-6">
+                <div class="form-group">
+  					<label for="sel1">Destination:</label>
+					  <select id="destination" class="form-control">
+						<option value="all" selected="selected"> All </option>
+		<?php
+           $sql = 'SELECT * FROM destination ORDER BY name';
+           foreach ($pdo->query($sql) as $row) {
+                    echo '<option value=" ' .$row['id']. ' ">'. $row['name'] . '</option>';
+           }
+        ?>
+					  </select>
+				</div>
+			</div>   
+        </div><!--end row-->
+        <div class="row">
+        	<div id="trips" class="col-xs-12">
 	        		
-        		</div>
-        	</div><!--end row-->
+        	</div>
+        </div><!--end row-->
     </div> <!-- /container -->
 
 
