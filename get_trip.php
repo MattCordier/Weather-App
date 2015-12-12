1d5c314843892f6164bac82e5cd5f33380560f1b
MattCordier <mcordier24@gmail.com>
2015-12-12 09:45:20 -0600

    pushing staging to AWS through Github

--- get_trip.php
+++ get_trip.php
@@ -5,7 +5,7 @@
                    $style = $_GET['style'];
                    $dest = $_GET['destination'];
 
-                   $sql = "SELECT * FROM trip";
+                   $sql = "SELECT * FROM trip JOIN image ON trip.id = trip_id";
 
 
                    	if($style!=="all"){
@@ -23,6 +23,7 @@
 
                    foreach ($pdo->query($sql) as $row) {
                    			echo '<div class= col-sm-4>';
+                            echo '<img>'. '$'. $row['image']; 
                             echo '<h2>'. $row['name'] . '</h2>';
                             echo '<p>'. $row['description'] . '</p>';
                             echo '<p>'. '$'. $row['cost'] . '</p>';
