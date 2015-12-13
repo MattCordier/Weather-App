fb08d66c2596b25e4b383a45df5ed18edfaf9ce9
MattCordier <mcordier24@gmail.com>
2015-12-13 15:03:05 -0600

    pushing staging to AWS through Github

--- get_trip.php
+++ get_trip.php
@@ -20,13 +20,13 @@
         $sql .= " ORDER BY trip.id DESC";
 
     				echo "<script>console.log('" . $sql . "');</script>";
-           
+
       foreach ($pdo->query($sql) as $row) {
        	echo '<div class= col-sm-4>';
         echo '<h2>'. $row['trip.name'] . '</h2>';
         echo '<p>'. $row['trip.description'] . '</p>';
         echo '<p>'. '$'. $row['trip.cost'] . '</p>';
-        echo '<img src=" '.data:image/jpeg;base64,$row['image.image']. '"/>';
+        echo '<img src="data:image/jpeg;base64,'.$row['image.image']. '"/>';
         echo '</div>';
                
        }
