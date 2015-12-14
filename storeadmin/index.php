579b4e0e79c69e17d1b2c004f5052be08504ad72
MattCordier <mcordier24@gmail.com>
2015-12-14 12:17:50 -0600

    pushing staging to AWS through Github

--- storeadmin/index.php
+++ storeadmin/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
-<?phpinclude 'http://ec2-52-32-68-207.us-west-2.compute.amazonaws.com/ecomm_connect.php';
+<?php
+include '../ecomm_connect.php';
 	$pdo = Database::connect();
 ?>
 <body>
