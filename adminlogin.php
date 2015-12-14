6b809e402f21933d149678456d7fa49bf3657eec
MattCordier <mcordier24@gmail.com>
2015-12-14 12:26:21 -0600

    pushing staging to AWS through Github

--- adminlogin.php
+++ adminlogin.php
@@ -5,8 +5,10 @@
 <?php require "header.php"; ?>
 <body>
 
-	<h1>YOU NEED TO LOGIN!!!</h1>
+
 <?php require "navigation.php";?>
+
+<h1>YOU NEED TO LOGIN!!!</h1>
 
 <?php require "footer.php";?>
 </body>
