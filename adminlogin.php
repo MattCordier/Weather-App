53a27dd3698f5e47ab262d373b00ec677400591a
MattCordier <mcordier24@gmail.com>
2015-12-14 13:47:39 -0600

    pushing staging to AWS through Github

--- adminlogin.php
+++ adminlogin.php
@@ -15,8 +15,7 @@ if(isset($_POST["username"])&&($_POST["password"])){
 $sql = mysql_query("SELECT * FROM customer WHERE username = '$manager' AND password = '$password' LIMIT 1");
 $existCount = mysql_num_rows($sql);
 if($existCount == 1){
-	
-	foreach (($pdo->query($sql) as $row)) {
+	foreach ($pdo->query($sql) as $row) {
 		$id = $row["id"];
 		}
 		$_SESSION["id"] = $id;
