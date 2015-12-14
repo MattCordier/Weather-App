12ef730871e451712cfde6538c78dda1a55eb0a5
MattCordier <mcordier24@gmail.com>
2015-12-14 13:54:16 -0600

    pushing staging to AWS through Github

--- adminlogin.php
+++ adminlogin.php
@@ -12,7 +12,7 @@ if(isset($_SESSION["manager"])){
 
 if(isset($_POST["username"])&&($_POST["password"])){
 
-$sql = mysql_query("SELECT * FROM customer WHERE username = '$manager' AND password = '$password' LIMIT 1");
+$sql = mysql_query("SELECT * FROM customer WHERE username = '$username' AND password = '$password' LIMIT 1");
 echo $sql;
 // $existCount = mysql_num_rows($sql);
 // 	if($existCount == 1){
