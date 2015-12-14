4b3d63c84260098885f16521cc21647027950511
MattCordier <mcordier24@gmail.com>
2015-12-14 14:03:16 -0600

    pushing staging to AWS through Github

--- adminlogin.php
+++ adminlogin.php
@@ -17,17 +17,18 @@ if(isset($_POST["manager"])&&($_POST["password"])){
 $sql = mysql_query("SELECT id FROM customer WHERE username = '$manager' AND password = '$password' LIMIT 1");
 
 $existCount = mysql_num_rows($sql);
-	if($existCount == 1){
-		echo "WHOO!";
-		// foreach ($pdo->query($sql) as $row) {
-		// 	$id = $row["id"];
-		// }
-		// $_SESSION["id"] = $id;
-		// $_SESSION["manager"] = $manager;
-		// $_SESSION["password"] = $password;
-		// header("location:admin.php");
-		exit();
-	} 
+echo $existCount;
+	// if($existCount == 1){
+	// 	echo "WHOO!";
+	// 	// foreach ($pdo->query($sql) as $row) {
+	// 	// 	$id = $row["id"];
+	// 	// }
+	// 	// $_SESSION["id"] = $id;
+	// 	// $_SESSION["manager"] = $manager;
+	// 	// $_SESSION["password"] = $password;
+	// 	// header("location:admin.php");
+	// 	exit();
+	// } 
 }
 
 ?>
