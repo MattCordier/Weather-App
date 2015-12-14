3178224101fe5c1b219c849e96d3bb8e3ed5db09
MattCordier <mcordier24@gmail.com>
2015-12-14 13:40:53 -0600

    pushing staging to AWS through Github

--- adminlogin.php
+++ adminlogin.php
@@ -12,21 +12,21 @@ if(isset($_SESSION["manager"])){
 
 if(isset($_POST["username"])&&($_POST["password"])){
 
-// $sql = mysql_query("SELECT * FROM customer WHERE id ='$managerID' AND username = '$manager' AND password = '$password' LIMIT 1");
-// $existCount = mysql_num_rows($sql);
-// if($existCount == 1){
-// 	foreach (($pdo->query($sql) as $row)) {
-// 		$id = $row["id"];
-// 	}
-// 	$_SESSION["id"] = $id;
-// 	$_SESSION["manager"] = $manager;
-// 	$_SESSION["password"] = $password;
-// 	header("location:admin.php");
-// 	exit();
-// } else {
-// 	echo "That information is incorrect, please try again<a href="adminlogin.php">Click Here</a>";
-// 	exit();
-// }
+$sql = mysql_query("SELECT * FROM customer WHERE id ='$managerID' AND username = '$manager' AND password = '$password' LIMIT 1");
+$existCount = mysql_num_rows($sql);
+if($existCount == 1){
+	foreach (($pdo->query($sql) as $row)) {
+		$id = $row["id"];
+	}
+	$_SESSION["id"] = $id;
+	$_SESSION["manager"] = $manager;
+	$_SESSION["password"] = $password;
+	header("location:admin.php");
+	exit();
+} else {
+	echo "That information is incorrect, please try again<a href="adminlogin.php">Click Here</a>";
+	exit();
+}
 }
 
 ?>
