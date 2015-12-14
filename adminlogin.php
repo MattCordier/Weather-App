28dd04995045400cead74b4d07953b73e425ca4d
MattCordier <mcordier24@gmail.com>
2015-12-14 13:24:29 -0600

    pushing staging to AWS through Github

--- adminlogin.php
+++ adminlogin.php
@@ -1,4 +1,10 @@
-
+<?php
+session_start();
+if(isset($_SESSION["manager"])){
+	header("location:admin.php");
+	exit();
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -8,15 +14,15 @@
 
 <?php require "navigation.php";?>
 
-<form id="login" name="login" method="post" action="admin.php">
+<form id="login" name="login" method="post" action="adminlogin.php">
 	User Name:<br/>
 		<input name="username" type="text" size="40">
 		<br/>
 	Password:<br/>
 		<input name="password" type="password" size="40">
-	<label>
+	
 		<input name="button" id="button" type="submit" value="Log In" size="40">
-	</label>
+	
 </form>
 
 <?php require "footer.php";?>
