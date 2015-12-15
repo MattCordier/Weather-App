0675f4bdc7c5e6ed933ce63b668193c49157b5d7
MattCordier <mcordier24@gmail.com>
2015-12-14 21:35:19 -0600

    pushing staging to AWS through Github

--- search.php
+++ search.php
@@ -4,23 +4,23 @@
 	include 'ecomm_connect.php';
     $pdo = Database::connect();
     $searchField = $_GET['searchField'];
-     
+    $search = "%".$searchField."%";
                 
 
     $sql = "SELECT * FROM trip WHERE name= ?";
     	$q = $pdo->prepare($sql);
-        $q->execute(array($searchField));
+        $q->execute(array($search));
         $data = $q->fetch(PDO::FETCH_ASSOC);
     	echo "<script>console.log('" . $sql . "');</script>";
 
-      foreach ($pdo->query($sql) as $row) {
-       	echo '<div class= col-sm-4>';
-        echo '<h2>'. $row['name'] . '</h2>';
-        echo '<p>'. $row['description'] . '</p>';
-        echo '<p>'. '$'. $row['cost'] . '</p>';
-        echo '</div>';
+      // foreach ($pdo->query($sql) as $row) {
+      //  	echo '<div class= col-sm-4>';
+      //   echo '<h2>'. $row['name'] . '</h2>';
+      //   echo '<p>'. $row['description'] . '</p>';
+      //   echo '<p>'. '$'. $row['cost'] . '</p>';
+      //   echo '</div>';
                
-       }
+      //  }
        Database::disconnect();
 
 
