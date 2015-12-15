89f2147bb6e4db130d4097dddf2d61ec41e6c7a3
MattCordier <mcordier24@gmail.com>
2015-12-14 21:08:58 -0600

    pushing staging to AWS through Github

--- search.php
+++ search.php
@@ -10,14 +10,14 @@
     $sql = "SELECT * FROM trip WHERE name= ". $searchField;
     	echo "<script>console.log('" . $sql . "');</script>";
 
-      // foreach ($pdo->query($sql) as $row) {
-      //  	echo '<div class= col-sm-4>';
-      //   echo '<h2>'. $row['name'] . '</h2>';
-      //   echo '<p>'. $row['description'] . '</p>';
-      //   echo '<p>'. '$'. $row['cost'] . '</p>';
-      //   echo '</div>';
+      foreach ($pdo->query($sql) as $row) {
+       	echo '<div class= col-sm-4>';
+        echo '<h2>'. $row['name'] . '</h2>';
+        echo '<p>'. $row['description'] . '</p>';
+        echo '<p>'. '$'. $row['cost'] . '</p>';
+        echo '</div>';
                
-      //  }
+       }
        Database::disconnect();
 
 
