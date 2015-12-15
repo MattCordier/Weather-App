c219ac97de9892541baf4e2452b98cf62483f285
MattCordier <mcordier24@gmail.com>
2015-12-14 21:30:39 -0600

    pushing staging to AWS through Github

--- search.php
+++ search.php
@@ -13,14 +13,14 @@
         $data = $q->fetch(PDO::FETCH_ASSOC);
     	echo "<script>console.log('" . $sql . "');</script>";
 
-      // foreach ($pdo->query($sql) as $row) {
-      //  	echo '<div class= col-sm-4>';
-      //   echo '<h2>'. $row['name'] . '</h2>';
-      //   echo '<p>'. $row['description'] . '</p>';
-      //   echo '<p>'. '$'. $row['cost'] . '</p>';
-      //   echo '</div>';
+      foreach ($pdo->query($sql) as $row) {
+       	echo '<div class= col-sm-4>';
+        echo '<h2>'. $row['name'] . '</h2>';
+        echo '<p>'. $row['description'] . '</p>';
+        echo '<p>'. '$'. $row['cost'] . '</p>';
+        echo '</div>';
                
-      //  }
+       }
        Database::disconnect();
 
 
