60124bf6e0fff0d7680e4e2bc412c8cc0ad6a4a0
MattCordier <mcordier24@gmail.com>
2015-12-14 20:59:01 -0600

    pushing staging to AWS through Github

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@
 
         function webSearch(){
            if(webSearch == ""){
-            $('#trip').show();
+            $('#trips').show();
            }
             var searchField = $('#srch-term').val();
            
