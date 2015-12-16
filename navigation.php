13f3a07770f54673fa275b8f6bfcebdb73b2bbc0
MattCordier <mcordier24@gmail.com>
2015-12-15 18:39:34 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -20,9 +20,7 @@
       <form class="navbar-form navbar-right" role="search">
         <div class="input-group">
             <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-            </div>
+  
         </div>
       </form>
       <ul class="nav navbar-nav navbar-right">
