ee1a421b36d3049c9dc7b78af0d196ccfd532c5a
MattCordier <mcordier24@gmail.com>
2015-12-15 20:59:25 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -26,13 +26,13 @@
       <ul class="nav navbar-nav navbar-right">
         <li><a href="settings.php">Settings</a></li>
         <li><a href="cart.php">Cart</a></li>
-        <li><a href="">Login</a></li>
-        <li><form>        
+        <li><a href="#">Login</a></li>
+      </ul>
+      <form class="navbar-form navbar-right">        
           <input type="text" value="" name="myinput" id="myinput"/>
           <input type="text" value="" name="myinput" id="myinput"/>
           <input type="submit"  name="submit" value="submit"/>
-      </form></li>
-      </ul>
+      </form>
       
          
       
