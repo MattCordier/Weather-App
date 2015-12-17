66516edeaac8588e09a07aa423e1860bf4a6cd8c
MattCordier <mcordier24@gmail.com>
2015-12-16 18:33:21 -0600

    pushing staging to AWS through Github

--- admin.php
+++ admin.php
@@ -5,17 +5,18 @@
 
 <body>
 <?php require "navigation.php";?>
+
 	<form id="login" action="authorize.php" method="POST" enctype="multipart/form-data">
 		User Name:
 		<input type="text" name="fn">
 		Password:
 		<input type="text" name="ln">
-		<input type="submit" value="Log In">
+		<input id="logbtn" class="btn" value="Log In">
 	</form>
 	
 
 
-	?>	
+	
 <?php Database::disconnect(); ?>
 <?php require "footer.php";?>
 </body>
