823f31e8b2bdd7f62ffa783697bb2f7b4e29616d
MattCordier <mcordier24@gmail.com>
2015-12-16 20:08:59 -0600

    pushing staging to AWS through Github

--- authorize.php
+++ authorize.php
@@ -8,7 +8,7 @@ $sql = "SELECT * FROM customer where username =". $username;
         $q = $pdo->prepare($sql);
         $q->execute(array($id));
         $data = $q->fetch(PDO::FETCH_ASSOC);
-        echo $data['firstname'];
+        return $data['firstname'];
         Database::disconnect();
 
         
