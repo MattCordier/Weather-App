0cf9dbd78e8293c4c6e2ed222ad2bf0419aa4778
MattCordier <mcordier24@gmail.com>
2015-12-16 20:21:34 -0600

    pushing staging to AWS through Github

--- authorize.php
+++ authorize.php
@@ -1,18 +1,15 @@
 <?php
 	include 'ecomm_connect.php';
     $pdo = Database::connect();
+	$username = $_GET['un'];
+	$password = $_GET['pw'];
+	$sql = "SELECT * FROM customer where username =". $username;
 
-$username = $_GET['un'];
-$password = $_GET['pw'];
-$sql = "SELECT * FROM customer where username =". $username;
-foreach ($pdo->query($sql) as $row) {
-       	echo '<div class= col-sm-4>';
-        echo '<h2>'. $row['id'] . '</h2>';
-        
-        echo '</div>';
-               
-       }
-        Database::disconnect();
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        echo $data['firstname'];
+    Database::disconnect();
 
         
 
