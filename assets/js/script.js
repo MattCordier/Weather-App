d2a3253e6405c67981aa75b0f5e0db9c93f9720a
MattCordier <mcordier24@gmail.com>
2015-12-20 15:54:57 -0600

    pushing staging to AWS through Github

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,9 @@
     		$.get("get_trip.php?style=" + style + "&destination=" + destination, function(data){
     			if(data !== null && data.length > 5){
     				$('#trips').html(data);
-    			} else if (data == ""){
+    			} else if (data.length < 5){
 
-					$('#trips').html(data + "We don't have any trips like that available at this time.");
+					$('#trips').html("We don't have any trips like that available at this time.");
     			}
     			
     		}); 
