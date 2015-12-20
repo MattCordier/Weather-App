2ede3cf3747aa05750c7b14843e99b121e13e470
MattCordier <mcordier24@gmail.com>
2015-12-20 15:26:28 -0600

    pushing staging to AWS through Github

--- settings.php
+++ settings.php
@@ -7,11 +7,11 @@
 <?php require "CRUD/trip/index.php";
 
 if(isset($_SESSION['userid']) && $_SESSION['permission'] === "Manager"){
-require "CRUD/customer/index.php";?>
+require "CRUD/customer/index.php";
 }
 
 
-
+?>
 
 <?php require "footer.php";?>
 </body>
