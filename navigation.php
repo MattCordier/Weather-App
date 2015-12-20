cfe72ee026a01ee22e435b3290fd2684b5413e08
MattCordier <mcordier24@gmail.com>
2015-12-20 12:45:03 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -24,7 +24,7 @@
         <li><a href="cart.php">Cart</a></li>
         <li><a href="login.php">Login</a></li>
         <li><?php if(isset($_SESSION['userid'])){
-                      echo "Hello, ". $_SESSION['firstname']. " ". $_SESSION['permision'];
+                      echo "Hello, ". $_SESSION['firstname']. " ". $_SESSION['permission'];
                     } ?></li>
         <li><form class="navbar-form navbar-right" role="search">
         <div class="input-group">
