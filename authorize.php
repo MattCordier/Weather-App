64be2806659c471f8665620ac972ea5a35c6a5f6
MattCordier <mcordier24@gmail.com>
2015-12-20 12:37:45 -0600

    pushing staging to AWS through Github

--- authorize.php
+++ authorize.php
@@ -34,13 +34,13 @@
     $_SESSION['permission'] = $permission
     $_SESSION['email'] = $email;
 
-    if ($_SESSION['permission'] === 1){
+    if ($_SESSION['permission'] == 1) {
         $_SESSION['permission'] = 'Manager';
-    } elseif ($_SESSION['permission' === 2){
+    } elseif ($_SESSION['permission' == 2){
         $_SESSION['permission'] = 'Guide';
-    } else{
-        $_SESSION['permission'] = 'Customer';
-    }
+      } else{
+         $_SESSION['permission'] = 'Customer';
+        }
     
     header('Location: index.php');
 
