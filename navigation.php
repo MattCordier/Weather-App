31459b82c6f99b623e04fac217da9fa5b1cc13e9
MattCordier <mcordier24@gmail.com>
2015-12-28 10:55:42 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php">Apex Trail Adventures</a>
+      <a class="navbar-brand" href="index.php"><img src="assests/img/apex_logo.png"></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
