daa5817d607e8a02688d7440c47718803fa06654
MattCordier <mcordier24@gmail.com>
2015-12-28 09:52:17 -0600

    pushing staging to AWS through Github

--- settings.php
+++ settings.php
@@ -4,6 +4,13 @@
 <body>
 <?php session_start()?>	
 <?php require "navigation.php";?>
+<ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
+
+
 <?php require "CRUD/trip/index.php";
 
 if(isset($_SESSION['userid']) && $_SESSION['permission'] === "Manager"){
