071af85aac44fee5b955a2dc3fcbfa44ee73ac2e
MattCordier <mcordier24@gmail.com>
2015-12-31 15:04:44 -0600

    pushing staging to AWS through Github

--- settings.php
+++ settings.php
@@ -23,7 +23,7 @@ if(isset($_SESSION['userid']) && $_SESSION['permission'] === "Guide"){
 	require "customer_index.php";
 	
 } elseif(isset($_SESSION['userid']) && $_SESSION['permission'] === "Customer"){
-	<div class="row">
+	echo '<div class="row">
                 <h3>Manage Customer</h3>
             </div>
             <div class="row">
@@ -40,31 +40,9 @@ if(isset($_SESSION['userid']) && $_SESSION['permission'] === "Guide"){
                       <th>Action</th>
                     </tr>
                   </thead>
-                  <tbody>
+                  <tbody>'
                  
-                 	include 'ecomm_connect.php';
-                   $pdo = Database::connect();
-                   $sql = 'SELECT * FROM customer WHERE id = $_SESSION["userid"]';
-                   foreach ($pdo->query($sql) as $row) {
-                            echo '<tr>';
-                            echo '<td>'. $row['firstname'] . '</td>';
-			                      echo '<td>'. $row['lastname'] . '</td>';
-                            echo '<td>'. $row['phone'] . '</td>';
-                            echo '<td>'. $row['dob'] . '</td>';
-                            echo '<td width=250>';
-                                echo '<a class="btn" href="customer_read.php?id='.$row['id'].'">Read</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-success" href="customer_update.php?id='.$row['id'].'">Update</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-danger" href="customer_delete.php?id='.$row['id'].'">Delete</a>';
-                                echo '</td>';
-                            echo '</tr>';
-                   }
-             		Database::disconnect();
-                 
-                  </tbody>
-            </table>
-        </div>
+                 	
 }
 
 
