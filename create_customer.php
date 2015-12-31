eb705e212c1c3429a7bb50502892d9ee5ebf331a
MattCordier <mcordier24@gmail.com>
2015-12-31 17:29:05 -0600

    pushing staging to AWS through Github

---
+++ create_customer.php
@@ -0,0 +1,216 @@
+<?php
+     
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $firstnameError = null;
+        $lastname = null;
+        $phoneError = null;
+        $dobError = null;
+        $usernameError = null;
+        $passwordError = null;
+        $genderError = null;
+        $permissionError = null;
+        $emailError = null;
+      
+         
+        // keep track post values
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $phone = $_POST['phone'];
+        $dob = $_POST['dob'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $gender = $_POST['gender'];
+        $permission = $_POST['permission'];
+        $email = $_POST['email'];
+         
+        // validate input
+        $valid = true;
+        if (empty($firstname)) {
+            $firstnameError = 'Please enter Name';
+            $valid = false;
+        }
+
+        if (empty($lastname)) {
+            $lastnameError = 'Please enter Last Name';
+            $valid = false;
+        }
+         
+        if (empty($phone)) {
+            $phoneError = 'Please enter Phone Number';
+            $valid = false;
+        } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+            $emailError = 'Please enter a valid Email Address';
+            $valid = false;
+        }
+         
+        if (empty($dob)) {
+            $dobError = 'Please enter Date of Birth';
+            $valid = false;
+        }
+
+        if (empty($username)) {
+            $usernameError = 'Please enter Username';
+            $valid = false;
+        }
+
+        if (empty($password)) {
+            $passwordError = 'Please enter Password';
+            $valid = false;
+        }
+
+        if (empty($gender)) {
+            $genderError = 'Please enter Gender';
+            $valid = false;
+        }
+
+        if (empty($permission)) {
+            $permissionError = 'Please enter permission';
+            $valid = false;
+        }
+
+        if (empty($email)) {
+            $emailError = 'Please enter Email';
+            $valid = false;
+        } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+            $emailError = 'Please enter a valid Email Address';
+            $valid = false;
+        }
+
+       
+         
+        // insert data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO customer (firstname, lastname, phone, dob, username, password, gender, permission, email) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($firstname, $lastname, $phone, $dob, $username, $password, $gender, $permission, $email));
+            Database::disconnect();
+            header("Location: index.php");
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Create a Customer</h3>
+                    </div>
+             
+                    <form class="form-horizontal" action="CRUD/customer/create_customer.php" method="post">
+
+
+                      <div class="control-group <?php echo !empty($firstnameError)?'error':'';?>">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input name="firstname" type="text"  placeholder="Name" value="<?php echo !empty($firstname)?$firstname:'';?>">
+                            <?php if (!empty($firstnameError)): ?>
+                                <span class="help-inline"><?php echo $firstnameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($lastnameError)?'error':'';?>">
+                        <label class="control-label">Last Name</label>
+                        <div class="controls">
+                            <input name="lastname" type="text"  placeholder="Last Name" value="<?php echo !empty($lastname)?$lastname:'';?>">
+                            <?php if (!empty($lastnameError)): ?>
+                                <span class="help-inline"><?php echo $lastnameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+
+
+                      <div class="control-group <?php echo !empty($phoneError)?'error':'';?>">
+                        <label class="control-label">Phone</label>
+                        <div class="controls">
+                            <input name="phone" type="text" placeholder="Phone Number" value="<?php echo !empty($phone)?$phone:'';?>">
+                            <?php if (!empty($phoneError)): ?>
+                                <span class="help-inline"><?php echo $phoneError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+
+                      <div class="control-group <?php echo !empty($dobError)?'error':'';?>">
+                        <label class="control-label">Date of Birth</label>
+                        <div class="controls">
+                            <input name="dob" type="text"  placeholder="Date of Birth" value="<?php echo !empty($dob)?$dob:'';?>">
+                            <?php if (!empty($dobError)): ?>
+                                <span class="help-inline"><?php echo $dobError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($usernameError)?'error':'';?>">
+                        <label class="control-label">Username</label>
+                        <div class="controls">
+                            <input name="username" type="text"  placeholder="Username" value="<?php echo !empty($username)?$username:'';?>">
+                            <?php if (!empty($usernameError)): ?>
+                                <span class="help-inline"><?php echo $usernameError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
+                        <label class="control-label">Password</label>
+                        <div class="controls">
+                            <input name="password" type="text"  placeholder="Password" value="<?php echo !empty($password)?$password:'';?>">
+                            <?php if (!empty($passwordError)): ?>
+                                <span class="help-inline"><?php echo $passwordError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($genderError)?'error':'';?>">
+                        <label class="control-label">Gender</label>
+                        <div class="controls">
+                            <input name="gender" type="text"  placeholder="Gender" value="<?php echo !empty($gender)?$gender:'';?>">
+                            <?php if (!empty($genderError)): ?>
+                                <span class="help-inline"><?php echo $genderError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($permissionError)?'error':'';?>">
+                        <label class="control-label">Permission</label>
+                        <div class="controls">
+                            <input name="permission" type="text"  placeholder="Permission" value="<?php echo !empty($permission)?$permission:'';?>">
+                            <?php if (!empty($permissionError)): ?>
+                                <span class="help-inline"><?php echo $permissionError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
+                        <label class="control-label">Email</label>
+                        <div class="controls">
+                            <input name="email" type="text"  placeholder="Email" value="<?php echo !empty($email)?$email:'';?>">
+                            <?php if (!empty($emailError)): ?>
+                                <span class="help-inline"><?php echo $emailError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Create</button>
+                          <a class="btn" href="index.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
