87c9d6fb5ecd861081fc488559c6d1d6ea14e604
MattCordier <mcordier24@gmail.com>
2015-12-31 11:49:52 -0600

    pushing staging to AWS through Github

--- settings.php
+++ settings.php
@@ -17,6 +17,7 @@ if(isset($_SESSION['userid']) && $_SESSION['permission'] === "Guide"){
 	require "destination_index.php";	
 } elseif(isset($_SESSION['userid']) && $_SESSION['permission'] === "Manager"){
 	require "trip_index.php";
+	require "destination_index.php";	
 	require "CRUD/customer/index.php";
 } elseif(isset($_SESSION['userid']) && $_SESSION['permission'] === "Customer"){
 	echo "Hello"; 
