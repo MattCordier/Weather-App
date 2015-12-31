157a72bd543b3c4add2af25423d11afad92718f2
MattCordier <mcordier24@gmail.com>
2015-12-31 15:18:10 -0600

    pushing staging to AWS through Github

--- settings.php
+++ settings.php
@@ -23,10 +23,10 @@ if(isset($_SESSION['userid']) && $_SESSION['permission'] === "Guide"){
 	require "customer_index.php";
 	
 } elseif(isset($_SESSION['userid']) && $_SESSION['permission'] === "Customer"){
-	echo '<div class="row">
+	echo '<div class="data">
                 <h3>Manage Customer</h3>
             </div>
-            <div class="row">
+            <div class="data">
             	<p>
                     <a href="create_customer.php" class="btn btn-success">Create</a>
                 </p>
@@ -45,21 +45,24 @@ if(isset($_SESSION['userid']) && $_SESSION['permission'] === "Guide"){
                  	include 'ecomm_connect.php';
                    $pdo = Database::connect();
                    $sql = 'SELECT * FROM customer WHERE id = $_SESSION["userid"]';
-                   foreach ($pdo->query($sql) as $row) {
+                   $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+                 
                             echo '<tr>';
-                            echo '<td>'. $row['firstname'] . '</td>';
-			                      echo '<td>'. $row['lastname'] . '</td>';
-                            echo '<td>'. $row['phone'] . '</td>';
-                            echo '<td>'. $row['dob'] . '</td>';
+                            echo '<td>'. $data['firstname'] . '</td>';
+			                echo '<td>'. $data['lastname'] . '</td>';
+                            echo '<td>'. $data['phone'] . '</td>';
+                            echo '<td>'. $data['dob'] . '</td>';
                             echo '<td width=250>';
-                                echo '<a class="btn" href="customer_read.php?id='.$row['id'].'">Read</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-success" href="customer_update.php?id='.$row['id'].'">Update</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-danger" href="customer_delete.php?id='.$row['id'].'">Delete</a>';
-                                echo '</td>';
+                            echo '<a class="btn" href="customer_read.php?id='.$data['id'].'">Read</a>';
+                            echo ' ';
+                            echo '<a class="btn btn-success" href="customer_update.php?id='.$data['id'].'">Update</a>';
+                            echo ' ';
+                            echo '<a class="btn btn-danger" href="customer_delete.php?id='.$data['id'].'">Delete</a>';
+                            echo '</td>';
                             echo '</tr>';
-                   }
+                   
              		Database::disconnect();
                  
                 echo  '</tbody>
