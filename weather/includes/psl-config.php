85d37623455922d7ee2dffbedd11d3574b0849f2
Matt Cordier <mcordier24@gmail.com>
2016-01-26 23:59:01 +0000

    php login tut

---
+++ weather/includes/psl-config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * These are the database login details
+ */  
+error_reporting(E_ALL);
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "sec_user");    // The database username. 
+define("PASSWORD", "eKcGZr59zAa2BEWUXXq131");    // The database password. 
+define("DATABASE", "secure_login");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
