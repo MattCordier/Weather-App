0b767d423cf33d3e84499ce3d27cca3c15953c94
MattCordier <mcordier24@gmail.com>
2016-01-26 21:49:50 -0600

    pushing staging to AWS through Github

--- weather/index.php
+++ weather/index.php
@@ -54,6 +54,7 @@ if (login_check($mysqli) == true) {
         <div id="login-modal" style="margin-top:50px;">
             <form action="includes/process_login.php" method="post" name="login_form">                      
                 Email: <input type="text" name="email" />
+                <br/>
                 Password: <input type="password" 
                                  name="password" 
                                  id="password"/>
