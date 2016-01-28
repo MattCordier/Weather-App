e7a7f9027ec76a974b8b1ec25b1436c843bc1cad
MattCordier <mcordier24@gmail.com>
2016-01-27 21:21:19 -0600

    pushing staging to AWS through Github

--- weather/js/maps.js
+++ weather/js/maps.js
@@ -1,7 +1,7 @@
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 42, lng: -84},
+          center: {lat: 43.0500, lng: 87.950},
           zoom: 8
         });
       }
