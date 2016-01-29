1010ad6fef35dc523ddd4b27da297155b8b4964f
MattCordier <mcordier24@gmail.com>
2016-01-29 11:28:26 -0600

    pushing staging to AWS through Github

--- weather/index.php
+++ weather/index.php
@@ -75,8 +75,8 @@ if (login_check($mysqli) == true) {
                 </div>
             </div>
             <div class="row weather-map" style="height:100%; width:100%;" >
-                <div class="col-sm-6">
-                    <div id="weather" class="weather-window">
+                <div class="col-sm-6" class="weather-window">
+                    <div id="weather" >
                         <h1>33 degrees</h1>
                     </div>
                 </div>
