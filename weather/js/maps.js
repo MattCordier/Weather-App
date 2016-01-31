7dd3a963e36bca3f85f173fd7a7d8236c6eab8a0
MattCordier <mcordier24@gmail.com>
2016-01-31 12:53:42 -0600

    pushing staging to AWS through Github

--- weather/js/maps.js
+++ weather/js/maps.js
@@ -23,7 +23,7 @@ function geocodeAddress(geocoder, resultsMap) {
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
       resultsMap.setCenter(results[0].geometry.location);
-      console.log(results[0].geometry.location);
+      console.log(results[0]);
       var marker = new google.maps.Marker({
         map: resultsMap,
         position: results[0].geometry.location
