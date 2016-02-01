c13977afd3e63b0e5f0e3b3074b394caa2f0527a
MattCordier <mcordier24@gmail.com>
2016-02-01 13:30:16 -0600

    pushing staging to AWS through Github

--- weather/js/maps.js
+++ weather/js/maps.js
@@ -1,89 +1,7 @@
 "use strict";
 var latlng;
 var date;
-function initMap() {
-  var customMapType = new google.maps.StyledMapType(mapStylesArray,{name: 'Custom Style'});
-  var customMapTypeId = 'custom_style';
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: {lat: 43, lng: -87},
-    mapTypeIds: {[google.maps.MapTypeId.ROADMAP, customMapTypeId]}
-  });
-  map.mapTypes.set(customMapTypeId, customMapType);
-  map.setMapTypeId(customMapTypeId)
-  var geocoder = new google.maps.Geocoder();
-
-  document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
-  });
-}
-
-function geocodeAddress(geocoder, resultsMap) {
-	latlng = [];	
-  var address = document.getElementById('address').value;
-  date = document.getElementById('datepicker').value;
-	geocoder.geocode({'address': address}, function(results, status) {
-	    if (status === google.maps.GeocoderStatus.OK) {
-	      resultsMap.setCenter(results[0].geometry.location);
-	      resultsMap.setZoom(8);
-	      latlng.push(results[0].geometry.location.lat());
-	      latlng.push(results[0].geometry.location.lng()):
-	      var marker = new google.maps.Marker({
-	        map: resultsMap, 
-	        position: results[0].geometry.location
-	      });
-	      
-	    } else {
-	      alert('Geocode was not successful for the following reason: ' + status);
-	    }
-	    if (date === ""){	
-			getWeather();  
-		} else if (date !== ""){
-			predictWeather();
-		}
-
-  	});
-	
-}
-
-// function getWeather(){
-// 	var apiKey = '8951bee95458c4ab8a6121ec2452207a';
-//     var url = 'https://api.forecast.io/forecast/';
-//     var lati = latlng[0];
-//     var longi = latlng[1];
-//     var data;
-
-//     $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
-//               console.log(data);
-//             $('#weather').html(data.daily.icon +'<h1>Current Temp: ' + data.currently.temperature + '&deg;</h1>');
-//         });
-
-// }
-
-// function predictWeather(){
-// 	var apiKey = '8951bee95458c4ab8a6121ec2452207a';
-//     var url = 'https://api.forecast.io/forecast/';
-//     var lati = latlng[0];
-//     var longi = latlng[1];
-//     var selectedDate = new Date(date);
-//     var time = selectedDate.getTime()/1000;
-//     console.log(time);
-//     console.log('the date is: '+ selectedDate);
-//     var data;
-
-//     $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + time + "?callback=?", function(data) {
-//               console.log(data);
-//             $('#weather').html(data.daily.icon +'<h1>Predicted: ' + data.currently.temperature + '&deg;</h1>');
-//         });
-// }
-
-
- $(function() {
-    $( "#datepicker" ).datepicker();
-  });  
-
-
- var mapStylesArray = [
+var mapStylesArray = [
     {
         "featureType": "all",
         "elementType": "labels",
@@ -310,3 +228,87 @@ function geocodeAddress(geocoder, resultsMap) {
         ]
     }
 ];
+
+function initMap() {
+  var customMapType = new google.maps.StyledMapType(mapStylesArray,{name: 'Custom Style'});
+  var customMapTypeId = 'custom_style';
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: {lat: 43, lng: -87},
+    mapTypeIds: {[google.maps.MapTypeId.ROADMAP, customMapTypeId]}
+  });
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId)
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+	latlng = [];	
+  var address = document.getElementById('address').value;
+  date = document.getElementById('datepicker').value;
+	geocoder.geocode({'address': address}, function(results, status) {
+	    if (status === google.maps.GeocoderStatus.OK) {
+	      resultsMap.setCenter(results[0].geometry.location);
+	      resultsMap.setZoom(8);
+	      latlng.push(results[0].geometry.location.lat());
+	      latlng.push(results[0].geometry.location.lng()):
+	      var marker = new google.maps.Marker({
+	        map: resultsMap, 
+	        position: results[0].geometry.location
+	      });
+	      
+	    } else {
+	      alert('Geocode was not successful for the following reason: ' + status);
+	    }
+	    if (date === ""){	
+			getWeather();  
+		} else if (date !== ""){
+			predictWeather();
+		}
+
+  	});
+	
+}
+
+// function getWeather(){
+// 	var apiKey = '8951bee95458c4ab8a6121ec2452207a';
+//     var url = 'https://api.forecast.io/forecast/';
+//     var lati = latlng[0];
+//     var longi = latlng[1];
+//     var data;
+
+//     $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
+//               console.log(data);
+//             $('#weather').html(data.daily.icon +'<h1>Current Temp: ' + data.currently.temperature + '&deg;</h1>');
+//         });
+
+// }
+
+// function predictWeather(){
+// 	var apiKey = '8951bee95458c4ab8a6121ec2452207a';
+//     var url = 'https://api.forecast.io/forecast/';
+//     var lati = latlng[0];
+//     var longi = latlng[1];
+//     var selectedDate = new Date(date);
+//     var time = selectedDate.getTime()/1000;
+//     console.log(time);
+//     console.log('the date is: '+ selectedDate);
+//     var data;
+
+//     $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + time + "?callback=?", function(data) {
+//               console.log(data);
+//             $('#weather').html(data.daily.icon +'<h1>Predicted: ' + data.currently.temperature + '&deg;</h1>');
+//         });
+// }
+
+
+ $(function() {
+    $( "#datepicker" ).datepicker();
+  });  
+
+
+ 
