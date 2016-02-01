45a7812fecf36143617997b9b8448f2843cdded8
MattCordier <mcordier24@gmail.com>
2016-02-01 13:47:59 -0600

    pushing staging to AWS through Github

--- weather/js/maps.js
+++ weather/js/maps.js
@@ -234,6 +234,7 @@ function initMap() {
   var customMapTypeId = 'custom_style';
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 2,
+    zoomControl: false,
     center: {lat: 43, lng: -20},
     mapTypeControlOptions: {
     	mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
