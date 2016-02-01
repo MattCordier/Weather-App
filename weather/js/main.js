93b352bbcd4a248c5797b6fcb925aaee09a4cfdd
MattCordier <mcordier24@gmail.com>
2016-02-01 16:05:56 -0600

    pushing staging to AWS through Github

--- weather/js/main.js
+++ weather/js/main.js
@@ -1,6 +1,7 @@
 $('#register').on('click', function(e){
 	e.preventDefault();
 	var url = this.href;
+	console.log(this.href);
 
 	$('#login-modal').remove();
 	$('#login-modal').load(url + ' html').hide().fadeIn('slow'); 
