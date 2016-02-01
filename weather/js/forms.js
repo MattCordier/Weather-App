f0c1898542df9ebd8e7acc861ab1d1f7c53ef31a
MattCordier <mcordier24@gmail.com>
2016-02-01 16:15:54 -0600

    pushing staging to AWS through Github

--- weather/js/forms.js
+++ weather/js/forms.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $('#login').on('click', function(){
         $('#login-modal').toggle("slow");
     });
+    
 
 
 });
--- weather/js/main.js
+++ weather/js/main.js
@@ -5,8 +5,9 @@ $('#register').on('click', function(e){
 
 	
 	$.ajax({
-  url: url
-}).done(function(data) { // data what is sent back by the php page
-  $('#login-modal').html(data).show(); // display data
-}); 
+  		url: url
+	}).done(function(data) { // data what is sent back by the php page
+  		$('#login-modal').html(data).show(); // display data
+	});
+	 
 });
