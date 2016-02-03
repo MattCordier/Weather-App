c7aa78f285234fb63720a34dc9211be13aabff6b
MattCordier <mcordier24@gmail.com>
2016-02-03 14:40:16 -0600

    pushing staging to AWS through Github

--- weather/index.php
+++ weather/index.php
@@ -26,7 +26,7 @@
         <div class="wrapper"> 
         <h1 id="app-title">Weather App</h1>      
         <div id="weather">
-            <div style="display:flex;  background-color:orange; height:500px;">
+            <div style="display:flex;  position: relative; background-color:orange; height:75%;">
                 <div id="weather-data" style="background-color:pink;">
                 <h2>Get the current weather for anywhere in the world!</h2>
                 </div>
