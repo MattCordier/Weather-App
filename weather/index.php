9393250f2247d94193562420cc48d0732617e447
MattCordier <mcordier24@gmail.com>
2016-02-03 11:33:29 -0600

    pushing staging to AWS through Github

--- weather/index.php
+++ weather/index.php
@@ -27,7 +27,6 @@
         <h1 id="app-title">Weather App</h1>      
         <div id="weather">
             <h2>Get the current weather for anywhere in the world!</h2>
-            <h2>sebfksbfkwbfkwbefkb jksfbljhbjhbwjfbwljhfbjw jhbfljhb we jlbflj ljbfljhbasljhfbwe ljhbslvkbs lkgrifhgrfuhierufhwef wfkherfb jrgiuhdvb fkhwerf</h2>
         </div>
         </div>
            
