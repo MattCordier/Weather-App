a7db6e011be01adf088d3e7aaba5d77256b46812
MattCordier <mcordier24@gmail.com>
2016-02-04 12:55:26 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -75,9 +75,9 @@ function getWeather(){
               console.log(data);
               currentContent += '<div class="six columns">';
               currentContent += '<h1 class="weather-current">' + Math.round(data.currently.temperature) + '&deg;</h1></div>';
-              currentContent += '<div class="six columns"><h4> High: ' + data.daily.data[0].temperatureMax + '&deg;</h4>';
-              currentContent += '<h4> Low: ' + data.daily.data[0].temperatureMin + '&deg;</h4>';
-              currentContent += '<h4> Percipitation: ' + (Math.floor(data.daily.data[0].precipProbability * 100)) + '&#37;</h4></div>';
+              currentContent += '<div class="six columns"><p> High: ' + data.daily.data[0].temperatureMax + '&deg;</p>';
+              currentContent += '<p> Low: ' + data.daily.data[0].temperatureMin + '&deg;</p>';
+              currentContent += '<p> Percipitation: ' + (Math.floor(data.daily.data[0].precipProbability * 100)) + '&#37;</p></div>';
             $('#weather-current').html(currentContent);
                 
 
