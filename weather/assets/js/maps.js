47b1af05034866a163d64c89dfe22078c31854da
MattCordier <mcordier24@gmail.com>
2016-02-05 16:53:10 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -11,10 +11,10 @@ function initMap() {
 
     date = "";
     if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) ){
+    navigator.geolocation.getCurrentPosition(function(position){
     latlng.push(position.coords.latitude);
     latlng.push(position.coords.longitude);
-    };
+    });
     
     console.log('Your latitude is :'+latlng[0]+' and longitude is '+latlng[1]);
 } else {
