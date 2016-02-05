a2cd02aa8391731cc5d59ac1df91136ce4396eed
MattCordier <mcordier24@gmail.com>
2016-02-05 13:26:20 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -8,12 +8,16 @@ var date;
 function initMap() {
     latlng = [];
     date = "";
-    if (navigator.geolocation) {
-        console.log('Geolocation is supported!');
-    }
-    else {
-        console.log('Geolocation is not supported for this Browser/OS version yet.');
-    }
+    // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
     // Set up basic map view
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
