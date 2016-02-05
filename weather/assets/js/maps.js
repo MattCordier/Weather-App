a7d19a79c1c8a7626e93cb7e0279fb2793087eef
MattCordier <mcordier24@gmail.com>
2016-02-05 16:02:48 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -19,7 +19,7 @@ function initMap() {
         zoom: 13,
         zoomControl: false,
         streetViewControl: false,
-        center: {lat: 43.0500, lng: -87.9500},
+        center: userLatLng,
         scrollwheel: false,
         mapTypeControl: false,
         mapTypeId: google.maps.MapTypeId.SATELLITE
