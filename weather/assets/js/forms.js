0a445fd87b5ed24c1c785ab19f2851c9511a74a4
MattCordier <mcordier24@gmail.com>
2016-02-05 04:13:37 -0600

    pushing staging to AWS through Github

--- weather/assets/js/forms.js
+++ weather/assets/js/forms.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    startclock();
     $('#login-modal').hide();
     $('#login').on('click', function(){
         $('#login-modal').toggle("slow");
@@ -28,33 +27,7 @@ $(document).ready(function() {
 
 });
 
-var timerID = null
-var timerRunning = false
 
-function stopclock(){
-    if(timerRunning)
-        clearTimeout(timerID)
-    timerRunning = false
-}
-
-function startclock(){
-    stopclock()
-    showtime()
-}
-
-function showtime(){
-    var now = new Date()
-    var hours = now.getHours()
-    var minutes = now.getMinutes()
-    var seconds = now.getSeconds()
-    var timeValue = "" + ((hours > 12) ? hours - 12 : hours)
-    timeValue  += ((minutes < 10) ? ":0" : ":") + minutes
-    timeValue  += ((seconds < 10) ? ":0" : ":") + seconds
-    timeValue  += (hours >= 12) ? " P.M." : " A.M."
-    document.clock.face.value = timeValue 
-    timerID = setTimeout("showtime()",1000)
-    timerRunning = true
-}
 
 
 function formhash(form, password) {
