8972af52f32a6e9f23e75c5882d29ea0772c6456
MattCordier <mcordier24@gmail.com>
2016-02-05 03:58:46 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -14,7 +14,7 @@ function initMap() {
         mapTypeId: google.maps.MapTypeId.SATELLITE
        
     });
-    // Keep Map Centered on resize
+    // Keep Map centered on resize
     google.maps.event.addDomListener(window, "resize", function() {
         var center = map.getCenter();
         google.maps.event.trigger(map, "resize");
@@ -22,11 +22,11 @@ function initMap() {
     });
     
     var geocoder = new google.maps.Geocoder();
-
+    //run if user taps submit
     document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder, map);
     });
-
+    //run if user hits enter
     $("#address").keypress(function(event) {
     if (event.which == 13) {
         event.preventDefault();
@@ -44,19 +44,12 @@ function geocodeAddress(geocoder, resultsMap) {
     geocoder.geocode({'address': address}, function(results, status) {
 	    if (status === google.maps.GeocoderStatus.OK) {
 	        resultsMap.setCenter(results[0].geometry.location);
-	       
 	        latlng.push(results[0].geometry.location.lat());
-	        latlng.push(results[0].geometry.location.lng());
-	        
-           
-	      
+	        latlng.push(results[0].geometry.location.lng()); 
 	    } else {
-	           alert('Geocode was not successful for the following reason: ' + status);
+	       alert('Geocode was not successful for the following reason: ' + status);
 	      }
-
-	    
-			predictWeather();
-		
+		predictWeather();		
     });
 }
 
@@ -108,39 +101,39 @@ function predictWeather(){
     var currentContent = "";
     var hourlyContent = "";
     var dailyContent = "";
+    var data;
 
+        //Check if datepicker(dp) has user input value 
         if (date !== ""){
             var selectedDate = new Date(date); 
             
         } else if (date === "") {
             var selectedDate = new Date();
         }
+
+        //convert selectedDate to Unix code
         var time = Math.floor(selectedDate.getTime()/1000);
     
     
-    //convert selectedDate to Unix code
-    
-    console.log(selectedDate);
-    console.log(time);
-    var data;
+    // console.log(selectedDate);
+    // console.log(time);
 
     $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + time + "?callback=?", function(data) {
               console.log(data);
               weatherHeader += '<div class="twelve columns weather-header-col"><p class="p-date">' + selectedDate.toDateString() +'</p></div>';
               
+              // 
+              // CONTENT FOR LEFT DIV, OVERVIEW
+              // 
               currentContent += '<div class="six columns">';
               currentContent += '<p class="weather-summary">' + data.currently.summary  + '&nbsp;&nbsp;|&nbsp;&nbsp;Feels like  ' + Math.round(data.currently.apparentTemperature) +'&deg;</p>'; 
               currentContent += '<h1 class="weather-current"><i class="wi wi-forecast-io-' + data.currently.icon + '"></i>' + Math.round(data.currently.temperature) + '&deg;</h1>';
               currentContent += '<p class="p-hilo"><span class="label-hilo">High: </span>' + Math.round(data.daily.data[0].temperatureMax) + '&deg;';
               currentContent += '<span class="label-hilo">&nbsp;&nbsp;&nbsp;Low: </span>' + Math.round(data.daily.data[0].temperatureMin) + '&deg;</p></div>';
-                      
-
-
-
-
-
-
-
+              
+              //
+              // CONTENT FOR RIGHT DIV, DETAILS
+              //         
               currentContent += '<div class="six columns weather-deets">';
               // currentContent += '<p><span class="label">Percipitation: </span>' + (Math.floor(data.daily.data[0].precipProbability * 100)) + '&#37;</p>';
                 currentContent += '<p><span class="label">Wind:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>' + (Math.floor(data.currently.windSpeed * 100)) + '&nbsp;mph</p>';
