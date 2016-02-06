0da9a63a44c5b6d734c345aaa05d78becc3f8acb
MattCordier <mcordier24@gmail.com>
2016-02-06 14:33:24 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -159,7 +159,7 @@ function predictWeather(){
                 currentContent += '<p><span class="label">Humidity:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>' + (Math.floor(data.currently.humidity * 100)) + '&#37;</p>';
                 currentContent += '<p><span class="label">Pressure:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>' + (Math.round(data.currently.pressure * 100)) + '&nbsp;mb</p>';
                 currentContent += '<div class="weather-deets-summary">';
-                currentContent += '<p><span class="label">Summary</span></p>'
+                currentContent += '<p><span class="label">Summary:</span></p>'
                 currentContent += '<p>' + data.hourly.summary + '</p></div>';
                 currentContent += '</div>';
             $('#weather-header').html(weatherHeader);
