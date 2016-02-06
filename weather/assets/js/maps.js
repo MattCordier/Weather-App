b7fe89dcf920af64fa3d7afb8d2f522d7c72b397
MattCordier <mcordier24@gmail.com>
2016-02-06 12:24:38 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -140,8 +140,9 @@ function predictWeather(){
               // CONTENT FOR LEFT DIV, OVERVIEW
               // 
               currentContent += '<div class="six columns">';
+              currentContent += '<i class="wi wi-forecast-io-' + data.currently.icon + '" title="'+ data.currently.icon + '"></i>'
               currentContent += '<p class="weather-summary">' + data.currently.summary  + '&nbsp;&nbsp;|&nbsp;&nbsp;Feels like  ' + Math.round(data.currently.apparentTemperature) +'&deg;</p>'; 
-              currentContent += '<h1 class="weather-current"><i class="wi wi-forecast-io-' + data.currently.icon + '" title="'+ data.currently.icon + '"></i>' + Math.round(data.currently.temperature) + '&deg;</h1>';
+              currentContent += '<h1 class="weather-current">' + Math.round(data.currently.temperature) + '&deg;</h1>';
               currentContent += '<p class="p-hilo"><span class="label-hilo">High: </span>' + Math.round(data.daily.data[0].temperatureMax) + '&deg;';
               currentContent += '<span class="label-hilo">&nbsp;&nbsp;&nbsp;Low: </span>' + Math.round(data.daily.data[0].temperatureMin) + '&deg;</p></div>';
               
