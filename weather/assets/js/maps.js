88673c6edc60fbed7b224fc81d53abb8b4aac709
MattCordier <mcordier24@gmail.com>
2016-02-06 13:37:55 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -99,7 +99,7 @@ function geocodeAddress(geocoder, resultsMap) {
                               city = addr.long_name + ", ";
                       }
                       
-                      $('#app-title').html(city + "   " + state + "   " + '<span style="font-weight: 300">' + country + '</span>');
+                      $('#app-title').html(city + "   " + state + "   " + '<span style="font-weight: 300italic">' + country + '</span>');
 
         } else {
            $('#alert').show().html('please enter a location');
