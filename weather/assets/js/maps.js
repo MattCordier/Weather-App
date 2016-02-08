7f489f42c34e8ac2a081058177e881e779cbd959
MattCordier <mcordier24@gmail.com>
2016-02-08 13:49:47 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -37,7 +37,7 @@ function success(position) {
    service.getDetails({placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'}, function(place, status){
       if (status === google.maps.places.PlacesServiceStatus.OK){
         $('#app-title').html(city + "   " + state + "   " + '<span style="font-weight: 300"><i>' + country + '</i></span>');
-      })
+      }
    });
 
     //Autocomplete address input
