567d570d88d755beda86de0fd26313204d3ec17f
MattCordier <mcordier24@gmail.com>
2016-02-09 21:35:00 -0600

    pushing staging to AWS through Github

--- weather/includes/process_login.php
+++ weather/includes/process_login.php
@@ -11,7 +11,7 @@ if (isset($_POST['email'], $_POST['p'])) {
  
     if (login($email, $password, $mysqli) == true) {
         // Login success 
-        header('Location: ../index.php');
+        header('Location: ../dashboard.php');
     } else {
         // Login failed 
         header('Location: ../index.php?error=1');
