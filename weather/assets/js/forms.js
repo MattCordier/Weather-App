64d4c8e246b4a8baa969a4ddf9741dd1a8a39203
MattCordier <mcordier24@gmail.com>
2016-02-09 21:12:20 -0600

    pushing staging to AWS through Github

--- weather/assets/js/forms.js
+++ weather/assets/js/forms.js
@@ -1,19 +1,7 @@
 $(document).ready(function() {
-    $('#login-modal').hide();
-    $('#login').on('click', function(){
-        $('#login-modal').toggle("slow");
-    });
+    
 
-    $('#register').on('click', function(e){
-        e.preventDefault();
-        var url = this.href;
-        console.log(this.href);
-        $.ajax({
-            url: url
-        }).done(function(data) { // data what is sent back by the php page
-            $('#login-modal').html(data).show(); // display data
-        });
-    });
+    
 
     $("#dp").datepicker({
         onSelect: function(dateText, inst) {
