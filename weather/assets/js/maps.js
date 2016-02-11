a32af15ae7d0ad6ae4499636f0cf22e9eaeab6f5
MattCordier <mcordier24@gmail.com>
2016-02-11 16:04:59 -0600

    pushing staging to AWS through Github

--- weather/assets/js/maps.js
+++ weather/assets/js/maps.js
@@ -149,8 +149,8 @@ function predictWeather() {
       currentContent += '<div class="six columns">';
       currentContent += '<i id="wi-current" class="wi wi-forecast-io-' + data.currently.icon + ' wi-big" title="'+ data.currently.icon + '"></i>'
       currentContent += '<h1  class="weather-current">' + Math.round(data.currently.temperature) + '&deg;</h1>';
-      currentContent += '<p id="hi-temp" class="p-hilo"><span class="label-hilo">High: </span>' + Math.round(data.daily.data[0].temperatureMax) + '&deg;';
-      currentContent += '<span "low-temp" class="label-hilo">&nbsp;&nbsp;&nbsp;Low: </span>' + Math.round(data.daily.data[0].temperatureMin) + '&deg;</p>';
+      currentContent += '<p class="p-hilo"><span id="hi-temp" class="label-hilo">High: </span>' + Math.round(data.daily.data[0].temperatureMax) + '&deg;';
+      currentContent += '<span id="lo-temp" class="label-hilo">&nbsp;&nbsp;&nbsp;Low: </span>' + Math.round(data.daily.data[0].temperatureMin) + '&deg;</p>';
       currentContent += '<p class="p-hilo"><span class="label-hilo">Feels like: </span>' + Math.round(data.currently.apparentTemperature) +'&deg;</p></div>';
       
       //
