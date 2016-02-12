6482d3cb0997f9b90f748bdf4d14bf31f3cad698
MattCordier <mcordier24@gmail.com>
2016-02-12 10:13:29 -0600

    pushing staging to AWS through Github

--- weather/assets/js/forms.js
+++ weather/assets/js/forms.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
         console.log(report);
         $.ajax({
             type : "POST",
-            url : 'weather/save-weather.php',
+            url : '../save-weather.php',
             data : report,
             success : function(data){
                 alert('yeah');
