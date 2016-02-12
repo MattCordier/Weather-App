dd347ebc2d1ce56bc7c39821db0842cbe58ec9bc
MattCordier <mcordier24@gmail.com>
2016-02-12 10:15:49 -0600

    pushing staging to AWS through Github

--- weather/save-weather.php
+++ weather/save-weather.php
@@ -4,20 +4,20 @@ ini_set('error_reporting', E_ALL);
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
 
-$error_msg = "";  
+// $error_msg = "";  
 
-if (isset($_POST['dpick'], $_POST['addy'])) {
-	$date = filter_input(INPUT_POST, 'dpick', FILTER_SANITIZE_STRING);
-	$address = filter_input(INPUT_POST, 'addy', FILTER_SANITIZE_STRING);
-} else {
-	$error_msg .= '<p class="error">The date or address entered is not vaild</p>'
-}
-	$high = $_POST['hi'];
-    $low = $_POST['lw'];
-    $summary = $_POST['smry'];
+// if (isset($_POST['dpick'], $_POST['addy'])) {
+// 	$date = filter_input(INPUT_POST, 'dpick', FILTER_SANITIZE_STRING);
+// 	$address = filter_input(INPUT_POST, 'addy', FILTER_SANITIZE_STRING);
+// } else {
+// 	$error_msg .= '<p class="error">The date or address entered is not vaild</p>'
+// }
+// 	$high = $_POST['hi'];
+//     $low = $_POST['lw'];
+//     $summary = $_POST['smry'];
 
-$insert_stmt = $mysqli->prepare("INSERT INTO locations (address, date, high, low, summary) VALUES (?, ?, ?, ?, ?)")
-$insert_stmt->bind_param($address, $date, $high, $low, $summary);
-$insert_stmt->execute();
-header('Location: index.php');
+// $insert_stmt = $mysqli->prepare("INSERT INTO locations (address, date, high, low, summary) VALUES (?, ?, ?, ?, ?)")
+// $insert_stmt->bind_param($address, $date, $high, $low, $summary);
+// $insert_stmt->execute();
+// header('Location: index.php');
 ?>
