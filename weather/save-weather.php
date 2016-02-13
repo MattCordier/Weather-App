9adaadb519b33c49a74d7bed546246cfd5ec5a55
MattCordier <mcordier24@gmail.com>
2016-02-12 18:11:34 -0600

    pushing staging to AWS through Github

--- weather/save-weather.php
+++ weather/save-weather.php
@@ -10,7 +10,7 @@ $high = $_POST['hi'];
 $low = $_POST['lw'];
 $summary = $_POST['smry']; 
 
-echo $date . " " . $address ." ". $summary . 'added to favorites';
+echo $date . " " . $address ." ". $summary . ' added to favorites';
 
 
 // if (isset($_POST['dpick'], $_POST['addy'])) {
