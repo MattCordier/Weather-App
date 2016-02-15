55aee98d3b3a82de0e719d3d186944fd23cc5ea3
MattCordier <mcordier24@gmail.com>
2016-02-15 15:06:14 -0600

    pushing staging to AWS through Github

--- weather/index.php
+++ weather/index.php
@@ -45,8 +45,8 @@ if (login_check($mysqli) == true) {
             <header id="main-header">
         <?php 
             if (login_check($mysqli) == true) {
-                echo '<p class="user-stats">Currently logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.&nbsp</p>';
-                echo '<p class="user-stats">Do you want to change user? <a href="includes/logout.php">Log out</a>.&nbsp</p>';
+                echo '<p class="user-stats">Logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.&nbsp</p>';
+                echo '<p class="user-stats">Change user? <a href="includes/logout.php">Log out</a>.&nbsp</p>';
             } else {
                 echo '<p class="user-stats">Currently logged ' . $logged . '.&nbsp</p>';
                 echo "<p class='user-stats'>Don't have an account? <a id='register' href='register.php'>Register here</a>&nbsp</p>";
