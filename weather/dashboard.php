a99135816032900c159f01136f7d66db8f8eae1f
MattCordier <mcordier24@gmail.com>
2016-02-16 10:20:26 -0600

    pushing staging to AWS through Github

--- weather/dashboard.php
+++ weather/dashboard.php
@@ -22,20 +22,14 @@ if (login_check($mysqli) == true) {
         <meta name="author" content="Matt Cordier">
         <meta name="description" content="A smart weather app for planning your travels.">
         <link rel="stylesheet" href="assets/css/skeleton.css">
-        <link rel="stylesheet" href="assets/css/jquery-ui.theme.min.css">
-        <link rel="stylesheet" href="assets/css/jquery-ui.structure.css">
         <link rel="stylesheet" type="text/styles" href="assets/css/styles.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">        
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">       
 
-        <link rel="stylesheet" type="text/styles" href="assets/css/weather-icons.min.css">
-        <link rel="stylesheet" type="text/styles" href="assets/css/weather-icons-wind.min.css">
-
-        <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>        <!-- <link rel="icon" type="image/png" href="assets/img/favicon.ico"> -->    
+        <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>        
+        <!-- <link rel="icon" type="image/png" href="assets/img/favicon.ico"> -->    
     </head>
-    <body class="dashboard-bg">
-        
-            
+    <body class="dashboard-bg">  
         <div class="wrapper"> 
             <header id="main-header">
         <?php 
@@ -66,58 +60,43 @@ if (login_check($mysqli) == true) {
                                 <th>Remove</th>
                             </tr>
                         </thead>
-                        <tbody>
-                  <?php
+                    <tbody>
+                <?php
                     include_once 'includes/db_connect.php';
                     include_once 'includes/psl-config.php';
-                   $stmt = $mysqli->prepare("SELECT * FROM locations ORDER BY ID DESC");
-                   $stmt->execute();
-                   $result = $stmt->get_result();
+                    $stmt = $mysqli->prepare("SELECT * FROM locations ORDER BY ID DESC");
+                    $stmt->execute();
+                    $result = $stmt->get_result();
 
-                   while($row = $result->fetch_assoc()) {
-                            echo '<tr>';                        
-                            echo '<td class="table-date">'. $row['date'] . '</td>';
-                            echo '<td>'. $row['address'] . '</td>';
-                            echo '<td>'. $row['high'] . '</td>';
-                            echo '<td>'. $row['low'] . '</td>';
-                            echo '<td>'. $row['summary'] . '</td>';
-                            echo '<td><button class="btn-remove" id='. $row['ID'] .'>X</button></td>';
-                            echo '</tr>';
+                    while($row = $result->fetch_assoc()) {
+                        echo '<tr>';                        
+                        echo '<td class="table-date">'. $row['date'] . '</td>';
+                        echo '<td>'. $row['address'] . '</td>';
+                        echo '<td>'. $row['high'] . '</td>';
+                        echo '<td>'. $row['low'] . '</td>';
+                        echo '<td>'. $row['summary'] . '</td>';
+                        echo '<td><button class="btn-remove" id='. $row['ID'] .'>X</button></td>';
+                        echo '</tr>';    
+                    }
+                    $result->close();
+                ?>
+                    </tbody>
+                    </table>
 
-                            
-                            // echo '<td width=250>';
-                            //     echo '<a class="btn" href="trip_read.php?id='.$row['id'].'">Read</a>';
-                            //     echo ' ';
-                            //     echo '<a class="btn btn-success" href="trip_update.php?id='.$row['id'].'">Update</a>';
-                            //     echo ' ';
-                            //     echo '<a class="btn btn-danger" href="trip_delete.php?id='.$row['id'].'">Delete</a>';
-                            //     echo '</td>';
-                            // echo '</tr>';
-                   }
-                   $result->close();
-                    // print_r($results);                  
-                    ?>
-                  </tbody>
-            </table>
-
-                </div>
-                <!-- <div class="six columns">beautiful</div> -->
-            </div>
-        </div>
+                </div><!--end column-->
+            </div><!--end row-->
+        </div><!--end container-->
 
         
     <footer id="main-footer">
             <p class="foot-deets">Weather App developed by <a href="http://mattcordier.github.io" target="_blank">Matt Cordier.</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Powered by <a href="http://forecast.io" target="_blank">Forecast.</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Matt Cordier &copy; 2016</p>
         </footer>
-        </div>
+    </div><!--end wrapper-->
            
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="assets/js/jquery-ui.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>
         <script src="assets/js/app.js"></script>
-        <script src="assets/js/maps.js"></script>
         <script type="text/JavaScript" src="assets/js/sha512.js"></script> 
         <script type="text/JavaScript" src="assets/js/forms.js"></script> 
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWIEjUkuuJx_OrEqaswU4SEYqaSl13Pek&libraries=places"></script>   
     </body>
 </html>   
