e37f3163cf5ba5c5d914671644ae37daee9bab0d
MattCordier <mcordier24@gmail.com>
2016-02-16 15:58:36 -0600

    pushing staging to AWS through Github

--- weather/register.php
+++ weather/register.php
@@ -8,12 +8,19 @@ include_once 'includes/functions.php';
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Secure Login: Registration Form</title>
-        <script type="text/JavaScript" src="assets/js/sha512.js"></script> 
-        <script type="text/JavaScript" src="assets/js/forms.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="assets/css/styles.css" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Matt Cordier">
+        <meta name="description" content="A smart weather app for planning your travels.">
+        <link rel="stylesheet" href="assets/css/skeleton.css">
+        <link rel="stylesheet" href="assets/css/jquery-ui.theme.min.css">
+        <link rel="stylesheet" href="assets/css/jquery-ui.structure.css">
+        <link rel="stylesheet" type="text/styles" href="assets/css/styles.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">        
+        <link rel="stylesheet" type="text/styles" href="assets/css/weather-icons.min.css">
+        <link rel="stylesheet" type="text/styles" href="assets/css/weather-icons-wind.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>        <!-- <link rel="icon" type="image/png" href="assets/img/favicon.ico"> -->    
     </head>
     <body class="register-bg">
         <div class="wrapper">
