73073f3328b1f7ba869e1cd198db490ca7ba00e4
MattCordier <mcordier24@gmail.com>
2016-02-15 20:21:56 -0600

    pushing staging to AWS through Github

--- weather/index.php
+++ weather/index.php
@@ -46,12 +46,12 @@ if (login_check($mysqli) == true) {
         <?php 
             if (login_check($mysqli) == true) {
                 echo '<p class="user-stats">Logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.&nbsp';
-                echo 'Change user? <a href="includes/logout.php">Log out</a>.&nbsp</p>';
-                echo '<a href="dashboard.php" class="login-link">login</a>';
+                echo 'Change user? <a href="includes/logout.php">Log out</a>.&nbsp';
+                echo '<a href="dashboard.php" class="login-link">login</a></p>';
             } else {
                 echo '<p class="user-stats">Currently logged ' . $logged . '.&nbsp';
-                echo "Don't have an account? <a id='register' href='register.php'>Register here</a>&nbsp</p>";
-                echo '<a href="dashboard.php" class="login-link">login</a>';
+                echo "Don't have an account? <a id='register' href='register.php'>Register here</a>&nbsp";
+                echo '<a href="dashboard.php" class="login-link">login</a></p>';
 
               }
         ?>   
