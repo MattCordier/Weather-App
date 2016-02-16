32a05d972c84b671ee4e3263846d8fe56f1bc590
MattCordier <mcordier24@gmail.com>
2016-02-15 18:07:04 -0600

    pushing staging to AWS through Github

--- weather/dashboard.php
+++ weather/dashboard.php
@@ -62,7 +62,7 @@ if (login_check($mysqli) == true) {
                   <?php
                     include_once 'includes/db_connect.php';
                     include_once 'includes/psl-config.php';
-                   $stmt = $mysqli->prepare("SELECT * FROM locations");
+                   $stmt = $mysqli->prepare("SELECT * FROM locations ORDER BY ID DESC");
 
                    $stmt->bind_param();
 
