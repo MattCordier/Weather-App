c0f504907d39827631ce9249fd3d535186e50e61
MattCordier <mcordier24@gmail.com>
2016-02-15 18:30:44 -0600

    pushing staging to AWS through Github

--- weather/dashboard.php
+++ weather/dashboard.php
@@ -75,7 +75,7 @@ if (login_check($mysqli) == true) {
                    while($row = $result->fetch_all()) {
                         print_r($row);
                         // $results[] = $row;
-                            // echo '<td>'. $row['date'] . '</td>';
+                            echo '<td>'. $row[0][3] . '</td>';
                             // echo '<td>'. $row['address'] . '</td>';
                             // echo '<td>'. $row['high'] . '</td>';
                             // echo '<td>'. $row['low'] . '</td>';
