4b11354bf0ab6c20d55c55bdbc0b0bcec968a596
MattCordier <mcordier24@gmail.com>
2016-02-15 20:25:18 -0600

    pushing staging to AWS through Github

--- weather/dashboard.php
+++ weather/dashboard.php
@@ -40,7 +40,7 @@ if (login_check($mysqli) == true) {
             if (login_check($mysqli) == true) {
                 echo '<p class="user-stats">Logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.&nbsp';
                 echo 'Change user? <a href="includes/logout.php">Log out</a>.&nbsp';
-                echo '<a href="dashboard.php" class="login-link">login</a></p>';
+                echo '<a href="index.php" class="login-link">view map</a></p>';
             } else {
                 echo '<p class="user-stats">Currently logged ' . $logged . '.&nbsp';
                 echo "Don't have an account? <a id='register' href='register.php'>Register here</a>&nbsp";
--- weather/index.php
+++ weather/index.php
@@ -47,11 +47,11 @@ if (login_check($mysqli) == true) {
             if (login_check($mysqli) == true) {
                 echo '<p class="user-stats">Logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.&nbsp';
                 echo 'Change user? <a href="includes/logout.php">Log out</a>.&nbsp';
-                echo '<a href="dashboard.php" class="login-link">login</a></p>';
+                echo '<a href="dashboard.php" class="login-link">my maps</a></p>';
             } else {
                 echo '<p class="user-stats">Currently logged ' . $logged . '.&nbsp';
                 echo "Don't have an account? <a id='register' href='register.php'>Register here</a>&nbsp";
-                echo '<a href="dashboard.php" class="login-link">login</a></p>';
+                echo '<a href="login.php" class="login-link">login</a></p>';
 
               }
         ?>   
