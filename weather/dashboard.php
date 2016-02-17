58d5eb3e50fab59eec658aff684fefb12e2707db
MattCordier <mcordier24@gmail.com>
2016-02-16 19:36:27 -0600

    pushing staging to AWS through Github

--- weather/dashboard.php
+++ weather/dashboard.php
@@ -81,7 +81,7 @@ if (login_check($mysqli) == true) {
                     //     echo '<td><button class="btn-remove" id='. $row['ID'] .'>X</button></td>';
                     //     echo '</tr>';    
                     // }
-                    echo $row[0];
+                    echo $row[1];
                     // $result->close();
                 ?>
                     </tbody>
