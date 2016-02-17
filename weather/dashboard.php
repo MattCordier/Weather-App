7d9094daa35396337c0b4c826fb8c1b02722e15d
MattCordier <mcordier24@gmail.com>
2016-02-16 20:09:42 -0600

    pushing staging to AWS through Github

--- weather/dashboard.php
+++ weather/dashboard.php
@@ -68,7 +68,7 @@ if (login_check($mysqli) == true) {
                     $count->execute();
                     $count_result = $count->get_result();
                     $data = $count_result->fetch_assoc();
-                    echo $data['members_id'];
+                    echo $data['members_id'][0];
                     
 
                     $stmt = $mysqli->prepare("SELECT * FROM locations WHERE members_id = ?");
