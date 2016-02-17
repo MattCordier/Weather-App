4fb24cf6c845434474037307b031fc4df08bfca2
MattCordier <mcordier24@gmail.com>
2016-02-17 14:01:58 -0600

    pushing staging to AWS through Github

--- weather/assets/js/forms.js
+++ weather/assets/js/forms.js
@@ -64,6 +64,21 @@ $(document).ready(function() {
         });
 
     })
+    $('.app-form .input-group input').focusout(function(){
+    
+    var text_val = $(this).val();
+    
+    if(text_val === "") {
+      
+      $(this).removeClass('has-value');
+      
+    } else {
+      
+      $(this).addClass('has-value');
+      
+    }
+    
+  });
 });
 
 function formhash(form, password) {
