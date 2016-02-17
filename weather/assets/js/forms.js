f48441b450380af2732a762b7eef8c1b628ba6eb
MattCordier <mcordier24@gmail.com>
2016-02-17 14:07:59 -0600

    pushing staging to AWS through Github

--- weather/assets/js/forms.js
+++ weather/assets/js/forms.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
 
 $(function(){
   
-  $('.app-form .input-group input').focusout(function(){
+  $('.app-form > .input-group > input').focusout(function(){
     
     var text_val = $(this).val();
     
