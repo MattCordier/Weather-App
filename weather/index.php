60555f28c2c0218cc37dfaccf5eb8e0563afd5e5
MattCordier <mcordier24@gmail.com>
2016-02-19 11:40:22 -0600

    geo test

--- weather/index.php
+++ weather/index.php
@@ -87,6 +87,6 @@ if (login_check($mysqli) == true) {
         <script src="assets/js/maps.js"></script>
         <script type="text/JavaScript" src="assets/js/sha512.js"></script> 
         <script type="text/JavaScript" src="assets/js/forms.js"></script> 
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWIEjUkuuJx_OrEqaswU4SEYqaSl13Pek&libraries=places"></script>   
+        <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBWIEjUkuuJx_OrEqaswU4SEYqaSl13Pek&libraries=places"></script>   
     </body>
 </html>
