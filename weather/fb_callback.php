cfbee61a845499035634c0b5fca23bb788201cdb
MattCordier <mcordier24@gmail.com>
2016-02-19 21:39:32 -0600

    oauth test

--- weather/fb_callback.php
+++ weather/fb_callback.php
@@ -51,11 +51,11 @@ $tokenMetadata = $oAuth2Client->debugToken($accessToken);
 echo '<h3>Metadata</h3>';
 var_dump($tokenMetadata);
 
-// // Validation (these will throw FacebookSDKException's when they fail)
-// $tokenMetadata->validateAppId({app-id}); // Replace {app-id} with your app id
-// // If you know the user ID this access token belongs to, you can validate it here
-// //$tokenMetadata->validateUserId('123');
-// $tokenMetadata->validateExpiration();
+// Validation (these will throw FacebookSDKException's when they fail)
+$tokenMetadata->validateAppId('1688543628082268'); // Replace {app-id} with your app id
+// If you know the user ID this access token belongs to, you can validate it here
+//$tokenMetadata->validateUserId('123');
+$tokenMetadata->validateExpiration();
 
 // if (! $accessToken->isLongLived()) {
 //   // Exchanges a short-lived access token for a long-lived one
