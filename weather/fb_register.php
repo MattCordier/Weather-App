a96d7a9a4ae10bfdddc25b0e27c43efa5bce950a
MattCordier <mcordier24@gmail.com>
2016-02-19 20:58:48 -0600

    oauth testing

--- weather/fb_register.php
+++ weather/fb_register.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 'on');
 
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
-include_once 'apikeys_include.php';
+include_once 'includes/apikeys_include.php';
 require __DIR__ . '/vendor/autoload.php';
 echo "Yesah";
 
