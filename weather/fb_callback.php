0f527db854812b1c234d58b2c840281ba2a31ec2
MattCordier <mcordier24@gmail.com>
2016-02-19 22:01:26 -0600

    oauth test

--- weather/fb_callback.php
+++ weather/fb_callback.php
@@ -77,7 +77,7 @@ $_SESSION['fb_access_token'] = (string) $accessToken;
 
 // User is logged in with a long-lived access token.
 // You can redirect them to a members-only page.
-header('Location: index.php');
+// header('Location: index.php');
 
 
 
