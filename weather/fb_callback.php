9f328e183a113f486569376642ace78c85fa4d13
MattCordier <mcordier24@gmail.com>
2016-02-19 21:23:17 -0600

    oauth test

--- weather/fb_callback.php
+++ weather/fb_callback.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
+include_once 'includes/db_connect.php';
+require __DIR__ . '/vendor/autoload.php';
 $fb = new Facebook\Facebook([
   'app_id' => '1688543628082268', // Replace {app-id} with your app id
   'app_secret' => '3ec6b0d0f7a566ebdd081634ac08748a',
