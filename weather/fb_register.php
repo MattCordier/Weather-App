90c9e21d9f94e2d822d364daeee9b2e9347580b0
MattCordier <mcordier24@gmail.com>
2016-02-19 21:08:29 -0600

    oauth testing

--- weather/fb_register.php
+++ weather/fb_register.php
@@ -6,9 +6,21 @@ ini_set('display_errors', 'on');
 
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
-include_once 'includes/apikeys_include.php';
 require __DIR__ . '/vendor/autoload.php';
-echo "Yesah";
+
+
+$fb = new Facebook\Facebook([
+  'app_id' => '1688543628082268', // Replace {app-id} with your app id
+  'app_secret' => '3ec6b0d0f7a566ebdd081634ac08748a',
+  'default_graph_version' => 'v2.2',
+  ]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+$permissions = ['email']; // Optional permissions
+$loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
+
+echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 
 
 ?>
