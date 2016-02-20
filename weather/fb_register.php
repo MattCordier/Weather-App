ecc4d4c7fb1bbf9c754116685047fac0b67d16c8
MattCordier <mcordier24@gmail.com>
2016-02-19 18:44:20 -0600

    adding fb register

---
+++ weather/fb_register.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+require __DIR__ . '/vendor/autoload.php';
+
+
+
+?>
