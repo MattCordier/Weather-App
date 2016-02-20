f5c9a2921dfad1561756210643483f0d7181d8b1
MattCordier <mcordier24@gmail.com>
2016-02-19 18:46:31 -0600

    adding fb register

--- weather/fb_register.php
+++ weather/fb_register.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 'on');
 
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
-require __DIR__ . '/vendo/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 echo "Yesah";
 
 
